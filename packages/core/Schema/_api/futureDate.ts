118ed83b8a7cf187e89a712b65561fe2fc7b1ed8
Patrick Roza <contact@patrickroza.com>
2021-06-07 17:40:16 +0200

    Fun with flags.

--- packages/core/Schema/_api/futureDate.ts
+++ packages/core/Schema/_api/futureDate.ts
@@ -2,18 +2,55 @@ import { pipe } from "@effect-ts-app/core/Function"
 
 import * as MO from "../_schema"
 import { domainEE, domainResponse2, onParseOrConstruct } from "../utils"
+import { Parser, These } from "../vendor"
 
 export const fromDateIdentifier = MO.makeAnnotation<{}>()
-export const fromDate: MO.DefaultSchema<Date, never, Date, Date, never, Date, {}> =
-  pipe(
-    MO.identity((u): u is Date => u instanceof Date),
-    MO.arbitrary((_) => _.date()),
-    MO.mapApi(() => ({})),
-    MO.withDefaults,
-    MO.annotate(fromDateIdentifier, {})
-  )
+export const fromDate: MO.DefaultSchema<
+  Date,
+  MO.LeafE<MO.ParseDateE>,
+  Date,
+  Date,
+  never,
+  Date,
+  {}
+> = pipe(
+  MO.identity((u): u is Date => u instanceof Date),
+  MO.arbitrary((_) => _.date()),
+  MO.mapApi(() => ({})),
+  MO.withDefaults,
+  MO.annotate(fromDateIdentifier, {})
+)
+
+export const fromStringOrDateIdentifier = MO.makeAnnotation<{}>()
+export const fromStringOrDate: MO.DefaultSchema<
+  string | Date,
+  MO.LeafE<MO.ParseDateE>,
+  Date,
+  Date,
+  never,
+  string,
+  {}
+> = pipe(
+  MO.identity((u): u is Date => u instanceof Date),
+  MO.parser((u) => (u instanceof Date ? These.succeed(u) : Parser.for(MO.date)(u))),
+  MO.arbitrary((_) => _.date()),
+  MO.encoder((_) => _.toISOString()),
+  MO.mapApi(() => ({})),
+  MO.withDefaults,
+  MO.annotate(fromStringOrDateIdentifier, {})
+)
 
 export const FutureDateFromDate = fromDate["|>"](
+  onParseOrConstruct((i) => {
+    const errors: MO.AnyError[] = []
+    if (i < new Date()) {
+      errors.push(domainEE("Date is not in the future"))
+    }
+    return domainResponse2(errors, () => i)
+  })
+)
+
+export const FutureDateFromStringOrDate = fromStringOrDate["|>"](
   onParseOrConstruct((i) => {
     const errors: MO.AnyError[] = []
     if (i < new Date()) {
