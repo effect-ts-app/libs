475bbcf0b52aabaa46a81bcf24823fd2454280e4
Patrick Roza <contact@patrickroza.com>
2021-06-27 10:29:16 +0200

    Fixups

--- utils/extract.sh
+++ utils/extract.sh
@@ -1,4 +1,14 @@
-for f in `find . -type f | grep .ts$ | grep -v .d.ts$`
+
+for d in `find . -type d$`
+do
+echo "\"${d}\": {
+    \"node\": \"${d}/index.js\",
+    \"import\": \"${d}/index.js\"
+  },"
+done
+
+
+for f in `find . -type f | grep .ts$ | grep -v index.ts$ | grep -v .d.ts$`
 do
 echo "\"${f%.ts}\": {
     \"node\": \"${f%.ts}.js\",
