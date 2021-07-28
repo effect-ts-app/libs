ea36e4b4e85594f6184a9aebf4a65650fdb553f7
Patrick Roza <contact@patrickroza.com>
2021-07-28 22:53:16 +0200

    prepare esm/js split

--- utils/extract.sh
+++ utils/extract.sh
@@ -1,16 +1,20 @@
 
-for d in `find . -type d$ | grep -v node_modules`
+for d in `find . -type d$ | grep -v node_modules | grep -v _esm/`
 do
-echo "\"${d}\": { \"module\": \"_esm/${d}/index.mjs\", \"default\": \"${d}/index.js\", },"
+echo "\"${d}\": { \"module\": \"./_esm${d#.}/index.js\", \"default\": \"${d}/index.js\", },"
 done
 
 
 for f in `find . -type f | grep .ts$ | grep -v index.ts$ | grep -v .d.ts$ | grep -v node_modules`
 do
-echo "\"${f%.ts}\": { \"module\": \"_esm/${f%.ts}.mjs\", \"default\": \"${f%.ts}.js\","
+f2="./_esm${f#.}"
+f2="${f2%.ts}.js"
+echo "\"${f%.ts}\": { \"module\": \"$f2\", \"default\": \"${f%.ts}.js\","
 done
 
 for f in `find . -type f | grep .tsx$ | grep -v index.ts$ | grep -v .d.ts$ | grep -v node_modules`
 do
-echo "\"${f%.tsx}\": { \"module\": \"_esm/${f%.tsx}.mjs\", \"default\": \"${f%.ts}.js\","
+f2="./_esm${f#.}"
+f2="${f2%.ts}.js"
+echo "\"${f2%.tsx}\": { \"module\": \"$f\", \"default\": \"${f%.tsx}.js\","
 done
--- utils/fix.sh
+++ utils/fix.sh
@@ -6,7 +6,7 @@ set -e
 VAR="
 {
   \"sideEffects\": false,
-  \"module\": \"./index.mjs\",
+  \"module\": \"./index.js\",
   \"typings\": \"./index.d.ts\"
 }
 "
@@ -19,24 +19,4 @@ do
   dir=$D
   #mkdir -p $dir
   echo $VAR > "${dir}/package.json"
-done
-
-# must use mv, as we are dealing with files in the same dir..
-
-# for f in `find . -type f | grep .d.ts$ | grep -v node_modules`
-# do
-#   #dest="../dist${f#.}"
-#   dest=$f
-#   mv -f -- "${f}.map" "${dest}.map"
-#   mv -f -- "$f" "${dest}"
-# done
-
-
-for f in `find _esm -type f | grep .js$ | grep -v .mjs | grep -v .eslintrc.js | grep -v node_modules`
-do
-  #dest="../dist${f#.}"
-  dest=$f
-  # the references to maps are not yet rewritten, so they stay ".js.map"
-  #mv -f -- "$f.map" "${dest%.js}.mjs.map"
-  mv -f -- "$f" "${dest%.js}.mjs"
 done
