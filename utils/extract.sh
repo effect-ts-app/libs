0468fa5d7bb4ec3c0a63621287eaabe31b100e78
Patrick Roza <contact@patrickroza.com>
2021-07-28 23:10:09 +0200

    update exports.

--- utils/extract.sh
+++ utils/extract.sh
@@ -1,7 +1,7 @@
 
 for d in `find . -type d$ | grep -v node_modules | grep -v _esm/`
 do
-echo "\"${d}\": { \"module\": \"./_esm${d#.}/index.js\", \"default\": \"${d}/index.js\", },"
+echo "\"${d}\": { \"module\": \"./_esm${d#.}/index.js\", \"default\": \"${d}/index.js\" },"
 done
 
 
@@ -9,12 +9,12 @@ for f in `find . -type f | grep .ts$ | grep -v index.ts$ | grep -v .d.ts$ | grep
 do
 f2="./_esm${f#.}"
 f2="${f2%.ts}.js"
-echo "\"${f%.ts}\": { \"module\": \"$f2\", \"default\": \"${f%.ts}.js\","
+echo "\"${f%.ts}\": { \"module\": \"$f2\", \"default\": \"${f%.ts}.js\" },"
 done
 
 for f in `find . -type f | grep .tsx$ | grep -v index.ts$ | grep -v .d.ts$ | grep -v node_modules`
 do
 f2="./_esm${f#.}"
 f2="${f2%.ts}.js"
-echo "\"${f2%.tsx}\": { \"module\": \"$f\", \"default\": \"${f%.tsx}.js\","
+echo "\"${f2%.tsx}\": { \"module\": \"$f\", \"default\": \"${f%.tsx}.js\" },"
 done
