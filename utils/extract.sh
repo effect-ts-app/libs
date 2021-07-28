d8fb86040fc2b7d87df82c0f1c6c768e53908658
Patrick Roza <contact@patrickroza.com>
2021-07-28 23:30:10 +0200

    Fixups

--- utils/extract.sh
+++ utils/extract.sh
@@ -1,11 +1,11 @@
 
-for d in `find . -type d$ | grep -v node_modules | grep -v _esm/`
+for d in `find . -type d$ | grep -v node_modules | grep -v _esm`
 do
 echo "\"${d}\": { \"module\": \"./_esm${d#.}/index.js\", \"default\": \"${d}/index.js\" },"
 done
 
 
-for f in `find . -type f | grep .ts$ | grep -v index.ts$ | grep -v .d.ts$ | grep -v node_modules`
+for f in `find . -type f | grep .ts$ | grep -v index.ts$ | grep -v .d.ts$ | grep -v .d.ts.map$ | grep -v node_modules`
 do
 f2="./_esm${f#.}"
 f2="${f2%.ts}.js"
@@ -15,6 +15,6 @@ done
 for f in `find . -type f | grep .tsx$ | grep -v index.ts$ | grep -v .d.ts$ | grep -v node_modules`
 do
 f2="./_esm${f#.}"
-f2="${f2%.ts}.js"
-echo "\"${f2%.tsx}\": { \"module\": \"$f\", \"default\": \"${f%.tsx}.js\" },"
+f2="${f2%.tsx}.js"
+echo "\"${f%.tsx}\": { \"module\": \"$f2\", \"default\": \"${f%.tsx}.js\" },"
 done
