b7b8ea6e1e87361623dd59769e18cc8563cd9dba
Patrick Roza <contact@patrickroza.com>
2021-09-08 12:03:40 +0200

    More lovely extensions

--- packages/core/Schema/_api/_shared.ts
+++ packages/core/Schema/_api/_shared.ts
@@ -10,15 +10,28 @@ import * as MO from "../_schema"
 //   }
 // }
 
-export function makeUtils<B, C, D, E>(self: MO.Schema<unknown, B, C, D, E>) {
+export function makeUtils<Schema extends MO.SchemaUPI>(self: Schema): Utils<Schema> {
   return {
     parse: EParserFor(self),
     unsafe: EParserFor(self)["|>"](MO.unsafe),
   }
 }
 
-export function extendWithUtils<B, C, D, E>(self: MO.Schema<unknown, B, C, D, E>) {
+export type Utils<Schema extends MO.SchemaUPI> = {
+  parse: EParserFor<Schema>
+  unsafe: UnsafeEParserFor<Schema>
+}
+
+export function extendWithUtils<Schema extends MO.SchemaUPI>(self: Schema) {
   return Object.assign(self, makeUtils(self))
+}
+
+export function extendWithUtilsAnd<Schema extends MO.SchemaUPI, Additional>(
+  self: Schema,
+  additional: (self: Schema & Utils<Schema>) => Additional
+) {
+  const extended = Object.assign(self, makeUtils(self))
+  return Object.assign(extended, additional(extended))
 }
 
 export type EParserFor<Self extends MO.SchemaAny> = MO.Parser.Parser<
@@ -26,6 +39,10 @@ export type EParserFor<Self extends MO.SchemaAny> = MO.Parser.Parser<
   MO.ParserErrorOf<Self>,
   MO.ParsedShapeOf<Self>
 >
+
+export type UnsafeEParserFor<Self extends MO.SchemaAny> = (
+  e: MO.EncodedOf<Self>
+) => MO.ParsedShapeOf<Self>
 
 export function EParserFor<ParsedShape, ConstructorInput, Encoded, Api>(
   schema: MO.Schema<unknown, ParsedShape, ConstructorInput, Encoded, Api>
