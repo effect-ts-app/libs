4afdb0a4a36d2681b059da2ddd12a41d6c72f947
Patrick Roza <contact@patrickroza.com>
2021-09-12 11:21:32 +0200

    Adapt for remaps

--- packages/fluent/src/polyfill/Prelude.ts
+++ packages/fluent/src/polyfill/Prelude.ts
@@ -1,12 +1,37 @@
 import { Case, Tagged } from "@effect-ts/core/Case"
 import * as XChunk from "@effect-ts/core/Collections/Immutable/Chunk"
 import * as XEffect from "@effect-ts/core/Effect"
+import {
+  die,
+  dieWith,
+  effectAsync,
+  effectAsyncInterrupt,
+  fail,
+  failWith,
+  halt,
+  haltWith,
+  succeed,
+  succeedWith,
+} from "@effect-ts/core/Effect"
 import * as XManaged from "@effect-ts/core/Effect/Managed"
 import { tag } from "@effect-ts/core/Has"
 
 const gl = global as any
 
-gl.Effect = XEffect
+gl.Effect = {
+  ...XEffect,
+  async: effectAsync,
+  asyncInterrupt: effectAsyncInterrupt,
+  do_: XEffect.do,
+  succeedNow: succeed,
+  succeed: succeedWith,
+  failNow: fail,
+  fail: failWith,
+  dieNow: die,
+  die: dieWith,
+  haltNow: halt,
+  halt: haltWith,
+}
 gl.Managed = XManaged
 gl.Chunk = XChunk
 gl.Data = {
