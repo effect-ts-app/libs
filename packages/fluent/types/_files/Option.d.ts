49e24e9bed3ca3031552bc5c223d2ef7b0515791
Patrick Roza <contact@patrickroza.com>
2021-09-13 10:55:00 +0200

    Option ops

--- packages/fluent/types/_files/Option.d.ts
+++ packages/fluent/types/_files/Option.d.ts
@@ -3,30 +3,27 @@ import type { IO as EffectIO } from "@effect-ts/core/Effect"
 import type { Option } from "@effect-ts/core/Option"
 import type { IO as SyncIO } from "@effect-ts/core/Sync"
 
-export interface OptionOps<A> {
-  /**
-   * @ets_rewrite_method alt_ from "@effect-ts-app/fluent/_ext/Option"
-   */
-  alt<A, B>(this: Option<A>, fb: () => Option<B>): Option<A | B>
-
-  /**
-   * @ets_rewrite_getter toNullable from "@effect-ts/core/Option"
-   */
-  readonly val: A | null
-
-  /**
-   * @ets_rewrite_method tryCatchOption_ from "@effect-ts-app/core/Sync"
-   */
-  encaseInSync<E, A>(this: Option<A>, onNone: () => E): SyncIO<E, A>
-
-  /**
-   * @ets_rewrite_method encaseOption_ from "@effect-ts-app/core/Effect"
-   */
-  encaseInEffect<E, A>(this: Option<A>, onNone: () => E): EffectIO<E, A>
-}
-
 declare module "@effect-ts/system/Option/core" {
-  interface Some<A> extends OptionOps<A> {}
-  interface None extends OptionOps<never> {}
+  export interface OptionOps {
+    /**
+     * @ets_rewrite_method alt_ from "@effect-ts-app/fluent/_ext/Option"
+     */
+    alt<A, B>(this: Option<A>, fb: () => Option<B>): Option<A | B>
+
+    /**
+     * @ets_rewrite_getter toNullable from "@effect-ts/core/Option"
+     */
+    readonly val: A | null
+
+    /**
+     * @ets_rewrite_method tryCatchOption_ from "@effect-ts-app/core/Sync"
+     */
+    encaseInSync<E, A>(this: Option<A>, onNone: () => E): SyncIO<E, A>
+
+    /**
+     * @ets_rewrite_method encaseOption_ from "@effect-ts-app/core/Effect"
+     */
+    encaseInEffect<E, A>(this: Option<A>, onNone: () => E): EffectIO<E, A>
+  }
 }
 //# sourceMappingURL=option.d.ts.map
