a72d53c8baac7c9a72ccf90a6f7eed302437bfc1
Patrick Roza <contact@patrickroza.com>
2022-01-11 13:45:48 +0100

    Fix Chunk changes

--- packages/core/Chunk.ts
+++ packages/core/Chunk.ts
@@ -1,8 +1,1 @@
-export {
-  collectChunk as collect,
-  collectChunkWithIndex as collectWithIndex,
-  collectChunk_ as collect_,
-  collectChunkWithIndex_ as collectWithIndex_,
-} from "@effect-ts/core/Collections/Immutable/Chunk"
-
 export * from "@effect-ts/core/Collections/Immutable/Chunk"
