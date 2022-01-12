a472b4c6db8f203c334af7fd4e9a92f8bb9d891c
Patrick Roza <contact@patrickroza.com>
2022-01-12 11:11:23 +0100

    Add Set.concat.

--- packages/core/Set.ts
+++ packages/core/Set.ts
@@ -2,6 +2,7 @@ import {
   fromArray as fromArray_,
   insert,
   insert_,
+  Set,
   toArray,
 } from "@effect-ts/core/Collections/Immutable/Set"
 import * as Eq from "@effect-ts/core/Equal"
@@ -10,6 +11,7 @@ import * as Ord from "./Order"
 
 export function make<A>(ord: Ord.Ord<A>, eq: Eq.Equal<A>) {
   const fromArray = fromArray_(eq)
+  const concat_ = (set: Set<A>, it: Iterable<A>) => fromArray([...set, ...it])
   return {
     insert: insert(eq),
     insert_: insert_(eq),
@@ -17,6 +19,8 @@ export function make<A>(ord: Ord.Ord<A>, eq: Eq.Equal<A>) {
     fromArray,
     from: (it: Iterable<A>) => fromArray([...it]),
     empty: () => new Set<A>(),
+    concat_,
+    concat: (it: Iterable<A>) => (set: Set<A>) => concat_(set, it),
   }
   // TODO: extend
 }
