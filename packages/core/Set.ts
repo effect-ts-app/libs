f2f1e57dfee34c3c42006fb45ff87eda23766979
Patrick Roza <contact@patrickroza.com>
2022-01-12 11:19:24 +0100

    Opaque schema extensions interface.

--- packages/core/Set.ts
+++ packages/core/Set.ts
@@ -9,7 +9,7 @@ import * as Eq from "@effect-ts/core/Equal"
 
 import * as Ord from "./Order"
 
-export function make<A>(ord: Ord.Ord<A>, eq: Eq.Equal<A>) {
+function make_<A>(ord: Ord.Ord<A>, eq: Eq.Equal<A>) {
   const fromArray = fromArray_(eq)
   const concat_ = (set: Set<A>, it: Iterable<A>) => fromArray([...set, ...it])
   return {
@@ -24,5 +24,16 @@ export function make<A>(ord: Ord.Ord<A>, eq: Eq.Equal<A>) {
   }
   // TODO: extend
 }
+
+class Wrapper<A> {
+  wrapped(ord: Ord.Ord<A>, eq: Eq.Equal<A>) {
+    return make_(ord, eq)
+  }
+}
+
+export interface SetSchemaExtensions<A> extends ReturnType<Wrapper<A>["wrapped"]> {}
+
+export const make: <A>(ord: Ord.Ord<A>, eq: Eq.Equal<A>) => SetSchemaExtensions<A> =
+  make_
 
 export * from "@effect-ts/core/Collections/Immutable/Set"
