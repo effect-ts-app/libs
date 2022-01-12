47a262223cc4b8748ccb5fd198269912c4cf1a86
Patrick Roza <contact@patrickroza.com>
2022-01-12 09:27:22 +0100

    Workaround conflicting star-exports warning

--- packages/core/Schema/index.ts
+++ packages/core/Schema/index.ts
@@ -461,4 +461,6 @@ export * from "./Model"
 export * from "./REST"
 export * from "./adapt"
 export * from "./_api"
+// workaround conflicting star-exports warning
+export { UUID } from "./_api"
 export * from "./_schema"
