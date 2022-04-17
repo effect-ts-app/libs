f595160df96d8685d8ae9aad2ee55a4da74e4cd7
Patrick Roza <contact@patrickroza.com>
2022-04-17 13:58:11 +0200

    yay

--- utils/rename.sh
+++ utils/rename.sh
@@ -5,9 +5,9 @@
 #   echo "$file" "${file%.js}.mjs"
 # done
 cd ./_esm
-find . -depth -name "*.d.ts" -exec sh -c 'mv "$1" ".${1}"' _ {} \;
+find . -depth -name "*.d.ts" -exec sh -c 'cp "$1" ".${1}"' _ {} \;
 find . -depth -name "*.d.ts.map" -exec sh -c 'sed -i "" "s/\.\.\/_src/_src/" $1' _ {} \;
-find . -depth -name "*.d.ts.map" -exec sh -c 'mv "$1" ".${1}"' _ {} \;
+find . -depth -name "*.d.ts.map" -exec sh -c 'cp "$1" ".${1}"' _ {} \;
 
 
 cd ../_cjs
