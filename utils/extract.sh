e3550d3776121c3c5b58a37348d3a6c01401bf1f
Patrick Roza <contact@patrickroza.com>
2022-07-14 09:20:08 +0200

    Cleanup

--- utils/extract.sh
+++ utils/extract.sh
@@ -1,20 +1,20 @@
 
 for d in `find . -type d$ | grep -v node_modules | grep -v _esm`
 do
-echo "\"${d}\": { \"import\": { \"types\": \"${d}/index.d.ts\", \"default\": \"./_esm${d#.}/index.mjs\" }, \"require\": \"${d}/index.js\" },"
+echo "\"${d}\": { \"import\": { \"types\": \"${d}/index.d.ts\", \"default\": \"./_mjs${d#.}/index.mjs\" }, \"require\": \"${d}/index.js\" },"
 done
 
 
 for f in `find . -type f | grep .ts$ | grep -v index.ts$ | grep -v .d.ts$ | grep -v .d.ts.map$ | grep -v node_modules`
 do
-f2="./_esm${f#.}"
+f2="./_mjs${f#.}"
 f2="${f2%.ts}.mjs"
 echo "\"${f%.ts}\": { \"import\": { \"types\": \"${f%.ts}.d.ts\", \"default\": \"$f2\" }, \"require\": \"${f%.ts}.js\" },"
 done
 
 for f in `find . -type f | grep .tsx$ | grep -v index.ts$ | grep -v .d.ts$ | grep -v node_modules`
 do
-f2="./_esm${f#.}"
+f2="./_mjs${f#.}"
 f2="${f2%.tsx}.mjs"
 echo "\"${f%.tsx}\": { \"import\": { \"types\": \"${f%.tsx}.d.ts\", \"default\": \"$f2\" }, \"require\": \"${f%.tsx}.js\" },"
 done
