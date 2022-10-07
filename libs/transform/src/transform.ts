716f07f7f705500a6ac26700519a9e37784f3cdd
Patrick Roza <contact@patrickroza.com>
2022-10-07 11:08:56 +0200

    Improve transform

--- libs/transform/src/transform.ts
+++ libs/transform/src/transform.ts
@@ -2,8 +2,36 @@ module.exports = function(fileInfo: { source: string }, api, options) {
   // transform `fileInfo.source` here
   // ...
   // return changed source
-  const source = fileInfo.source
-    .replaceAll('"@effect-ts/core/Effect"', '"@effect/core/io/Effect"')
-    .replaceAll(/RIO<\s*(\w+),\s*(\w+)>/g, (_, args) => `Effect<${args[0]}, never, ${args[1]}>`)
+  const moduleMap = {
+    "@effect-ts/core/Effect": "@effect/core/io/Effect",
+    "@effect-ts/core/Effect/Layer": "@effect/core/io/Layer",
+    "@effect-ts/core/Effect/Fiber": "@effect/core/io/Fiber",
+    "@effect-ts/core/Effect/Cause": "@effect/core/io/Cause",
+    "@effect-ts/system/Cause": "@effect/core/io/Cause",
+    "@effect-ts/core/Has": "@tsplus/stdlib/service/Tag",
+    "@effect-ts/system/Has": "@tsplus/stdlib/service/Tag",
+    "@effect-ts/core/Support/AtomicBoolean": "@tsplus/stdlib/data/AtomicBoolean",
+    "@effect-ts/core/Effect/Supervisor": "@effect/core/io/Supervisor",
+    "@effect-ts/core/Function": "@tsplus/stdlib/data/Function",
+    "@effect-ts/system/Function": "@tsplus/stdlib/data/Function"
+  }
+
+  // TODO
+  // import type { _A, _R } from "@effect-ts/system/Utils"
+  // _A<  = T.Success<
+
+  // TODO: scope
+  //const removedModules = ["@effect-ts/core/Effect/Managed"]
+  let source = fileInfo.source
+  for (const k of Object.keys(moduleMap)) {
+    source = source.replaceAll(`"${k}"`, `"${moduleMap[k]}"`)
+  }
+  source = source
+    .replaceAll(/RIO<\s*(\w+),\s*(\w+)>/g, (_, ...args) => `Effect<${args[0]}, never, ${args[1]}>`)
+    .replaceAll(/& Has<(\w+)>/g, (_, ...m) => `| ${m[0]}`)
+    .replaceAll(/Has<(\w+)>/g, (_, ...m) => `${m[0]}`)
+    .replaceAll(".succeedWith(", ".sync(")
+    .replaceAll(/.effectAsync([(<])/g, (_, m) => `.async${m[0]}`)
+  .replaceAll(".chain", ".flatMap")
   return source;
 };
