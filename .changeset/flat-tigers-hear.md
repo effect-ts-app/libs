387dea6eaf54ce7103c4f94368864e7ee9ec96b0
Patrick Roza <contact@patrickroza.com>
2023-01-23 09:53:25 +0100

    fix: Cache MapKey should not override Hash & Equal. fixes stackoverflow

---
+++ .changeset/flat-tigers-hear.md
@@ -0,0 +1,5 @@
+---
+"@effect-app/prelude": patch
+---
+
+Fix Cache MapKey
--- packages/prelude/_src/vendor/Cache/Cache/_internal/MapKey.ts
+++ packages/prelude/_src/vendor/Cache/Cache/_internal/MapKey.ts
@@ -1,6 +1,3 @@
-import * as Equal from "@fp-ts/data/Equal"
-import * as Hash from "@fp-ts/data/Hash"
-
 export const MapKeyURI = Symbol.for("@effect/cache/Cache/MapKey")
 export type MapKeyURI = typeof MapKeyURI
 
@@ -9,31 +6,13 @@ export type MapKeyURI = typeof MapKeyURI
  * to the previous key and next key in the `KeySet` to support an efficient
  * implementation of a sorted set of keys.
  */
-export class MapKey<Key> implements Equal.Equal {
+export class MapKey<Key> {
   readonly [MapKeyURI]: MapKeyURI = MapKeyURI
 
   previous: MapKey<Key> | undefined = undefined
   next: MapKey<Key> | undefined = undefined
 
   constructor(readonly value: Key) {}
-
-  [Hash.symbol](): number {
-    return Hash.combine(
-      Hash.combine(
-        Hash.hash(this.next)
-      )(Hash.hash(this.previous))
-    )(Hash.hash(this.value))
-  }
-
-  [Equal.symbol](that: unknown): boolean {
-    if (this === that) {
-      return true
-    }
-    return isMapKey(that) &&
-      Equal.equals(this.value, that.value) &&
-      Equal.equals(this.previous, that.previous) &&
-      Equal.equals(this.next, that.next)
-  }
 }
 
 /**
