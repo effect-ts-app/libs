02997e8f0fe6e3eb582952d628e77374d076262a
Patrick Roza <contact@patrickroza.com>
2023-02-07 13:33:27 +0100

    fix pjson

---
+++ .changeset/happy-chicken-search.md
@@ -0,0 +1,11 @@
+---
+"@effect-app/infra-adapters": patch
+"@effect-app/compiler": patch
+"@effect-app/prelude": patch
+"@effect-app/schema": patch
+"@effect-app/infra": patch
+"@effect-app/react": patch
+"@effect-app/cli": patch
+---
+
+FUP
--- scripts/package.js
+++ scripts/package.js
@@ -8,20 +8,25 @@ const items = JSON.parse(`{${s.substring(0, s.length - 1)} }`)
 
 const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"))
 const exps = {
-  ".": {
-    "import": {
-      "types": "./dist/index.d.ts",
-      "default": "./dist/index.js"
-    },
-    "require": {
-      "types": "./dist/index.d.ts",
-      "default": "./_cjs/index.cjs"
+  ...fs.existsSync("./_src/index.ts")
+  ? {
+    ".": {
+      "import": {
+        "types": "./dist/index.d.ts",
+        "default": "./dist/index.js"
+      },
+      "require": {
+        "types": "./dist/index.d.ts",
+        "default": "./_cjs/index.cjs"
+      }
     }
-  },
-  ...items,
-  // ...pkg.name === "@effect-app/core" ? {
-  //   "./types/awesome": { "types": "./types/awesome.d.ts" }
-  // } : {},
+  }
+  : undefined,
+...Object.keys(items)
+  .reduce((prev, cur) => {
+    if (cur !== "./index") prev[cur] = items[cur]
+    return prev
+  }, {})
 }
 console.log(exps)
 pkg.exports = exps
