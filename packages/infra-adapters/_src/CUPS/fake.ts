c507165c9d20b9e2432befdd536800c68cccce85
Patrick Roza <contact@patrickroza.com>
2023-02-13 11:46:17 +0100

    add fake CUPS.

---
+++ packages/infra-adapters/_src/CUPS/fake.ts
@@ -0,0 +1,19 @@
+import { pretty } from "@effect-app/core/utils"
+import { CUPS } from "./service.js"
+
+const makeFakeCups = Effect.sync(() => {
+  return {
+    print: (buffer, printerId, ...options) =>
+      Effect.logInfo("Printing to fake printer")
+        .zipRight(Effect({ stdout: "fake", stderr: "" }))
+        .logAnnotate("printerId", printerId)
+        .logAnnotate("options", pretty(options))
+        .logAnnotate("bufferSize", buffer.byteLength.toString()),
+    getAvailablePrinters: Effect([])
+  } satisfies CUPS
+})
+
+/**
+ * @tsplus static CUPS.Ops Fake
+ */
+export const FAKECups = makeFakeCups.toLayer(CUPS)
