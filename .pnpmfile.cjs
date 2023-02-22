36546494f324b8116238e5e01c0f6a2176900373
Patrick Roza <contact@patrickroza.com>
2023-02-22 12:11:16 +0100

    FUP

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -3,9 +3,9 @@ module.exports = {
         ...require("./packages/pnpm-singleton/_cjs/index.cjs").makeHooks({ specificPackages: [
             "@tsplus/stdlib",
             "@effect/cache",
-            "@effect/io",
-            "@fp-ts/codec",
             "@effect/data",
+            "@effect/io",
+            "@fp-ts/optic",
             "@fp-ts/schema",
             "date-fns"
             //"vue",
