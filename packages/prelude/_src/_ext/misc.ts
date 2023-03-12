eba972208f3792ac4c8fddc2c51e828de68eb196
Patrick Roza <contact@patrickroza.com>
2023-03-12 21:06:19 +0100

    cleanup

--- packages/prelude/_src/_ext/misc.ts
+++ packages/prelude/_src/_ext/misc.ts
@@ -41,39 +41,38 @@ type LowerFirst<S extends PropertyKey> = S extends `${infer First}${infer Rest}`
 type LowerServices<T extends Record<string, Tag<any>>> = { [key in keyof T as LowerFirst<key>]: Service<T[key]> }
 
 /**
- * @tsplus static effect/io/Effect.Ops servicesWith
+ * @tsplus static effect/io/Effect.Ops services
  */
 export function accessLowerServices_<T extends Record<string, Tag<any>>, A>(
+  services: T
+) {
+  return Effect.all(
+    services.$$.keys.reduce((prev, cur) => {
+      prev[((cur as string)[0]!.toLowerCase() + (cur as string).slice(1)) as unknown as LowerFirst<typeof cur>] = Effect
+        .service(services[cur]!)
+      return prev
+    }, {} as any)
+  ) as any as Effect<Values<T>, never, LowerServices<T>>
+}
+
+/**
+ * @tsplus static effect/io/Effect.Ops servicesWith
+ */
+export function accessLowerServicesWith_<T extends Record<string, Tag<any>>, A>(
   services: T,
   fn: (services: LowerServices<T>) => A
 ) {
-  return Debug.untraced(() =>
-    (Effect.all(
-      services.$$.keys.reduce((prev, cur) => {
-        prev[((cur as string)[0]!.toLowerCase() + (cur as string).slice(1)) as unknown as LowerFirst<typeof cur>] =
-          Effect.service(services[cur]!)
-        return prev
-      }, {} as any)
-    ) as any as Effect<Values<T>, never, LowerServices<T>>).map(fn)
-  )
+  return Debug.untraced(() => accessLowerServices_(services).map(fn))
 }
 
 /**
  * @tsplus static effect/io/Effect.Ops servicesWithEffect
  */
-export function accessLowerServicesEffect_<T extends Record<string, Tag<any>>, R, E, A>(
+export function accessLowerServicesWithEffect_<T extends Record<string, Tag<any>>, R, E, A>(
   services: T,
   fn: (services: LowerServices<T>) => Effect<R, E, A>
 ) {
-  return Debug.untraced(() =>
-    (Effect.all(
-      services.$$.keys.reduce((prev, cur) => {
-        prev[((cur as string)[0]!.toLowerCase() + (cur as string).slice(1)) as unknown as LowerFirst<typeof cur>] =
-          Effect.service(services[cur]!)
-        return prev
-      }, {} as any)
-    ) as any as Effect<Values<T>, never, LowerServices<T>>).flatMap(fn)
-  )
+  return Debug.untraced(() => accessLowerServices_(services).flatMap(fn))
 }
 
 // /**
