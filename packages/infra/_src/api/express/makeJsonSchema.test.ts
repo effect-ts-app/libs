b6af4d2f9970ae5812dd5cca3a37051a4dc9ccad
Andrea Simone Costa <jfet97@users.noreply.github.com>
2023-06-21 11:49:18 +0200

    Enhance paths check (#45)
    * separate subpaths from params to avoid possible clashing with numerical subpaths
    
    * check shadowed paths
    
    * wip
    
    * now it works but it sucks
    
    * effectify teh tings
    
    * minor fix
    
    * changeset
    
    ---------
    
    Co-authored-by: Patrick Roza <contact@patrickroza.com>

--- packages/infra/_src/api/express/makeJsonSchema.test.ts
+++ packages/infra/_src/api/express/makeJsonSchema.test.ts
@@ -1,5 +1,5 @@
 import { InvalidStateError } from "../../errors.js"
-import { checkDuplicatePaths } from "./makeJsonSchema.js"
+import { checkPaths } from "./makeJsonSchema.js"
 
 const path = { path: "/securities/:id/search/:id2", method: "get" }
 
@@ -14,8 +14,9 @@ const differentPaths: {
   path: string
   method: string
 }[] = [
-  "/securities/:id/search/",
   "/securities/search/:id2",
+  "/securities/:id/search/",
+  "/securities/:id/search/:id2",
   "/securities/:id/:id2/search/:id3"
 ]
   .map((path) => ({ path, method: "get" }))
@@ -24,12 +25,37 @@ const differentPaths: {
   ]
     .map((path) => ({ path, method: "post" })))
 
-test("works", () => {
-  expect(checkDuplicatePaths([path, ...differentPaths])).toStrictEqual(Effect.succeed([path, ...differentPaths]))
+test("works", async () => {
+  expect(await (checkPaths(differentPaths)).runPromise).toStrictEqual(differentPaths)
 
-  equalPaths.forEach((p) =>
-    expect(checkDuplicatePaths([path, p])).toStrictEqual(
-      Effect.fail(new InvalidStateError(`Duplicate method ${p.method} for path ${p.path}`))
+  for (const p of equalPaths) {
+    expect(await checkPaths([path, p]).runPromiseEither).toStrictEqual(
+      Either.left(new InvalidStateError(`Duplicate method ${p.method} for path ${p.path}`))
     )
-  )
+  }
+
+  expect(await checkPaths(["a/:p1/c/:p2", "a/b"].map((path) => ({ path, method: "get" }))).runPromiseEither)
+    .toStrictEqual(
+      Either.left(new InvalidStateError(`Path /a/b/ is shadowed by /a/:p1/`))
+    )
+
+  expect(await checkPaths(["a/:p1/c/:p2", "a/:p111/c/d"].map((path) => ({ path, method: "get" }))).runPromiseEither)
+    .toStrictEqual(
+      Either.left(new InvalidStateError(`Path /a/:p1/c/d/ is shadowed by /a/:p1/c/:p2/`))
+    )
+})
+
+test("log shadowing", async () => {
+  const arr: string[] = []
+  const paths = ["a/b", "a/:p"].map((path) => ({ path, method: "get" }))
+
+  const CustomLogger = Logger.make<string, void>((_, __, message) => {
+    arr.push(message)
+  })
+
+  await checkPaths(paths)
+    .provideLayer(Logger.replace(Logger.defaultLogger, CustomLogger))
+    .runPromise
+
+  expect(arr).toStrictEqual([`Path /a/:p/ is partially shadowed by /a/b/`])
 })
--- packages/infra/_src/api/express/makeJsonSchema.ts
+++ packages/infra/_src/api/express/makeJsonSchema.ts
@@ -9,62 +9,118 @@ const rx = /:(\w+)/g
 
 type _A<C> = C extends ReadonlyArray<infer A> ? A : never
 
-export function checkDuplicatePaths<T extends { path: string; method: string }>(
-  paths: readonly T[]
-): Effect<never, InvalidStateError, readonly T[]> {
-  const methods = Symbol("methods")
-  interface PathMethods {
-    [key: string | number]: this | undefined
-    [methods]: string[]
+class PathsTree {
+  public readonly methods: string[]
+  public readonly params: { [key: number]: PathsTree | undefined }
+  public readonly subpaths: { [key: string]: PathsTree | undefined }
+  public readonly path: string
+
+  constructor(path: string) {
+    this.methods = []
+    this.params = {}
+    this.subpaths = {}
+    this.path = path
   }
-  const pathMethods: PathMethods = { [methods]: [] }
-  const regex = /(?:^|\/)([^/:\n]+)|:(\w+)/g
+}
 
-  for (const path of paths) {
-    const matches = [...path.path.matchAll(regex)]
-      .map((match) =>
-        match[1]
-          ? { _tag: "resource", value: match[1] } as const
-          : { _tag: "param", value: match[2]! } as const
-      )
+interface Match {
+  _tag: "resource" | "param"
+  value: string
+}
 
-    let pathNavigator: PathMethods | string[] = pathMethods
-    for (let i = 0; i < matches.length;) {
-      const match = matches[i]!
-      switch (match._tag) {
-        case "resource": {
-          if (!(match.value in (pathNavigator))) {
-            ;(pathNavigator)[match.value] = { [methods]: [] }
-          }
-          pathNavigator = (pathNavigator)[match.value]!
-          i++
-          break
+function checkShadowing(
+  pathNavigator: PathsTree,
+  match: Match
+) {
+  return Effect.gen(function*($) {
+    switch (match._tag) {
+      case "resource": {
+        // check shadowing: if 'a/:param' comes before 'a/b', then 'a/b' is shadowed
+        if (pathNavigator.params[1]) {
+          return yield* $(Effect.fail(
+            new InvalidStateError(
+              `Path ${pathNavigator.path}${match.value}/ is shadowed by ${pathNavigator.params[1].path}`
+            )
+          ))
         }
-        case "param": {
-          let numberOfParams = 1
-          while (i < matches.length && matches[i + 1]?._tag === "param") {
-            numberOfParams++
-            i++
-          }
-
-          if (!(numberOfParams in pathNavigator)) {
-            ;(pathNavigator)[numberOfParams] = { [methods]: [] }
-          }
-          pathNavigator = (pathNavigator)[numberOfParams]!
-          i++
-          break
+        break
+      }
+      case "param": {
+        // check shadowing: if 'a/b' comes before 'a/:param', then 'a/"param' is partially shadowed
+        const subpaths = Object.getOwnPropertyNames(pathNavigator.subpaths)
+        if (subpaths.length > 0) {
+          yield* $(subpaths.forEachEffect((s) =>
+            Effect.logInfo(
+              `Path ${pathNavigator.path}:${match.value}/ is partially shadowed by ${pathNavigator.subpaths[s]!.path}`
+            )
+          ))
         }
+        break
       }
     }
+  })
+}
 
-    if (pathNavigator[methods].includes(path.method)) {
-      // throw duplicate path-method error
-      return Effect.fail(new InvalidStateError(`Duplicate method ${path.method} for path ${path.path}`))
+export function checkPaths<T extends { path: string; method: string }>(
+  paths: readonly T[]
+) {
+  return Effect.gen(function*($) {
+    const pathMethods: PathsTree = new PathsTree("/")
+    const regex = /(?:^|\/)([^/:\n]+)|:(\w+)/g
+    for (const path of paths) {
+      const matches = [...path.path.matchAll(regex)]
+        .map((match) =>
+          match[1]
+            ? { _tag: "resource", value: match[1] } as const
+            : { _tag: "param", value: match[2]! } as const
+        )
+
+      let pathNavigator = pathMethods
+      for (let i = 0; i < matches.length;) {
+        const match = matches[i]!
+
+        yield* $(checkShadowing(pathNavigator, match))
+
+        switch (match._tag) {
+          case "resource": {
+            if (!(match.value in pathNavigator.subpaths)) {
+              pathNavigator.subpaths[match.value] = new PathsTree(`${pathNavigator.path}${match.value}/`)
+            }
+
+            pathNavigator = pathNavigator.subpaths[match.value]!
+            i++
+            break
+          }
+          case "param": {
+            const paramsNames = [match.value]
+            let numberOfParams = 1
+            while (i < matches.length && matches[i + 1]?._tag === "param") {
+              numberOfParams++
+              i++
+              paramsNames.push(matches[i]!.value)
+            }
+
+            if (!(numberOfParams in pathNavigator.params)) {
+              pathNavigator.params[numberOfParams] = new PathsTree(
+                `${pathNavigator.path}:${paramsNames.join("/:")}/`
+              )
+            }
+            pathNavigator = pathNavigator.params[numberOfParams]!
+            i++
+            break
+          }
+        }
+      }
+
+      if (pathNavigator.methods.includes(path.method)) {
+        // throw duplicate path-method error
+        return yield* $(Effect.fail(new InvalidStateError(`Duplicate method ${path.method} for path ${path.path}`)))
+      }
+      pathNavigator.methods.push(path.method)
     }
-    pathNavigator[methods].push(path.method)
-  }
 
-  return Effect.succeed(paths)
+    return paths
+  })
 }
 
 /**
@@ -74,7 +130,7 @@ export function makeJsonSchema(r: Iterable<RS.RouteDescriptorAny>) {
   return Chunk
     .fromIterable(r)
     .forEachEffect(RS.makeFromSchema)
-    .flatMap(checkDuplicatePaths)
+    .flatMap(checkPaths)
     .map((e) => {
       const map = ({ method, path, responses, ...rest }: _A<typeof e>) => ({
         [method]: {
