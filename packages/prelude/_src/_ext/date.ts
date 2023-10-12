9181c1019f62f17122e82a00adef410630fe62d9
Patrick Roza <contact@patrickroza.com>
2023-10-12 09:53:21 +0200

    feat: extend Date extensions

--- packages/prelude/_src/_ext/date.ts
+++ packages/prelude/_src/_ext/date.ts
@@ -1,4 +1,19 @@
-import { addDays, addHours, subDays, subHours } from "date-fns"
+import {
+  addDays,
+  addHours,
+  addMinutes,
+  addMonths,
+  addSeconds,
+  addWeeks,
+  addYears,
+  subDays,
+  subHours,
+  subMinutes,
+  subMonths,
+  subSeconds,
+  subWeeks,
+  subYears
+} from "date-fns"
 
 /**
  * @tsplus fluent Date addDays
@@ -17,3 +32,53 @@ export const DateAddHours: (date: Date, amount: number) => Date = addHours
  * @tsplus fluent Date subHours
  */
 export const DateSubHours: (date: Date, amount: number) => Date = subHours
+
+/**
+ * @tsplus fluent Date addMinutes
+ */
+export const DateAddMinutes: (date: Date, amount: number) => Date = addMinutes
+
+/**
+ * @tsplus fluent Date subMinutes
+ */
+export const DateSubMinutes: (date: Date, amount: number) => Date = subMinutes
+
+/**
+ * @tsplus fluent Date addSeconds
+ */
+export const DateAddSeconds: (date: Date, amount: number) => Date = addSeconds
+
+/**
+ * @tsplus fluent Date subSeconds
+ */
+export const DateSubSeconds: (date: Date, amount: number) => Date = subSeconds
+
+/**
+ * @tsplus fluent Date addYears
+ */
+export const DateAddYears: (date: Date, amount: number) => Date = addYears
+
+/**
+ * @tsplus fluent Date subYears
+ */
+export const DateSubYears: (date: Date, amount: number) => Date = subYears
+
+/**
+ * @tsplus fluent Date addMonths
+ */
+export const DateAddMonths: (date: Date, amount: number) => Date = addMonths
+
+/**
+ * @tsplus fluent Date subMonths
+ */
+export const DateSubMonths: (date: Date, amount: number) => Date = subMonths
+
+/**
+ * @tsplus fluent Date addWeeks
+ */
+export const DateAddWeeks: (date: Date, amount: number) => Date = addWeeks
+
+/**
+ * @tsplus fluent Date subWeeks
+ */
+export const DateSubWeeks: (date: Date, amount: number) => Date = subWeeks
