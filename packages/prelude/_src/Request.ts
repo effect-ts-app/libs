a05e0735e2d802a34ec672a734029e4092b8fbf8
Patrick Roza <contact@patrickroza.com>
2023-10-21 15:02:57 +0200

    fix HttpClient global

--- packages/prelude/_src/Request.ts
+++ packages/prelude/_src/Request.ts
@@ -1,2 +1,5 @@
+// there are issues with global imports with "as", so we re export here
 export { Cache as EffectCache } from "effect/Cache"
 export { Request as EffectRequest } from "effect/Request"
+
+export { Client as HttpClient } from "@effect/platform/Http/Client"
