01b08c7225fcc3f89ae9633dc62d6fb59a98e6e0
Patrick Roza <contact@patrickroza.com>
2023-11-15 15:19:28 +0100

    fix id

--- packages/prelude/_src/ids.ts
+++ packages/prelude/_src/ids.ts
@@ -3,11 +3,12 @@ import { brandedStringId, type StringIdBrand } from "@effect-app/prelude/schema"
 export interface RequestIdBrand extends StringIdBrand {
   readonly RequestId: unique symbol
 }
+
 /**
  * @tsplus type RequestId
  */
-export type RequestId = ReasonableString & RequestIdBrand
-export const RequestId = brandedStringId<RequestId>()
+export type RequestId = ReasonableString
+export const RequestId = Object.assign(ReasonableString, { withDefault: defaultProp(ReasonableString, StringId.make) })
 
 export interface UserProfileIdBrand extends StringIdBrand {
   readonly UserProfileId: unique symbol
