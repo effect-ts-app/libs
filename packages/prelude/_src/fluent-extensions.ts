9c3fc59ded5258671817d0da79007f1e5425594b
Patrick Roza <contact@patrickroza.com>
2023-11-30 14:55:25 +0100

    cleanup

--- packages/prelude/_src/fluent-extensions.ts
+++ packages/prelude/_src/fluent-extensions.ts
@@ -47,11 +47,16 @@ export const installFluentExtensions = <R>(runtime: Runtime<R>) => {
 /*
 import type * as Runtime from "effect/Runtime"
 import type * as Fiber from "effect/Fiber"
+import type { RT } from "./effect-runtime"
+
 declare module "effect/Effect" {
   export interface Effect<R, E, A> {
     get runPromise(this: Effect<RT, E, A>): Promise<A>
     get runSync(this: Effect<RT, E, A>): A
-    runFork<E, A>(this: Effect<RT, E, A>, options?: Runtime.RunForkOptions): Fiber.RuntimeFiber<E, A>
+    runFork<E, A>(
+      this: Effect<RT, E, A>,
+      options?: Runtime.RunForkOptions,
+    ): Fiber.RuntimeFiber<E, A>
   }
 }
 */
