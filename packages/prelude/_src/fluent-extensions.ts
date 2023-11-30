d4a6be261df6151640ed1c0a3170759021a93452
Patrick Roza <contact@patrickroza.com>
2023-11-30 12:41:27 +0100

    fup

--- packages/prelude/_src/fluent-extensions.ts
+++ packages/prelude/_src/fluent-extensions.ts
@@ -1,6 +1,8 @@
-import type {} from "./fluent-extensions-ext.js"
+import type * as Cause from "effect/Cause"
 import * as Effect from "effect/Effect"
 import * as RT from "effect/Runtime"
+import type * as Runtime from "effect/Runtime"
+import type { NoInfer } from "effect/Types"
 
 /**
  * useful in e.g frontend projects that do not use tsplus, but still has the most useful extensions installed.
@@ -39,4 +41,53 @@ export const installFluentExtensions = <R>(runtime: Runtime<R>) => {
       return Effect.tap(this, arg)
     }
   })
+}
+
+// Put the following in the project, where RT is the default runtime context available
+/*
+import type * as Runtime from "effect/Runtime"
+import type * as Fiber from "effect/Fiber"
+declare module "effect/Effect" {
+  export interface Effect<R, E, A> {
+    get runPromise(this: Effect<RT, E, A>): Promise<A>
+    get runSync(this: Effect<RT, E, A>): A
+    runFork<E, A>(this: Effect<RT, E, A>, options?: Runtime.RunForkOptions): Fiber.RuntimeFiber<E, A>
+  }
+}
+*/
+
+declare module "effect/Option" {
+  export interface None<out A> {
+    get value(): A | undefined
+  }
+}
+
+declare module "effect/Effect" {
+  export interface Effect<R, E, A> {
+    andThen<A, R, E, X>(
+      this: Effect<R, E, A>,
+      f: (a: NoInfer<A>) => X
+    ): [X] extends [Effect<infer R1, infer E1, infer A1>] ? Effect<R | R1, E | E1, A1>
+      : [X] extends [Promise<infer A1>] ? Effect<R, Cause.UnknownException | E, A1>
+      : Effect<R, E, X>
+
+    andThen<A, R, E, X>(
+      this: Effect<R, E, A>,
+      f: X
+    ): [X] extends [Effect<infer R1, infer E1, infer A1>] ? Effect<R | R1, E | E1, A1>
+      : [X] extends [Promise<infer A1>] ? Effect<R, Cause.UnknownException | E, A1>
+      : Effect<R, E, X>
+    tap<A, R, E, X>(
+      this: Effect<R, E, A>,
+      f: (a: NoInfer<A>) => X
+    ): [X] extends [Effect<infer R1, infer E1, infer _A1>] ? Effect<R | R1, E | E1, A>
+      : [X] extends [Promise<infer _A1>] ? Effect<R, Cause.UnknownException | E, A>
+      : Effect<R, E, A>
+    tap<A, R, E, X>(
+      this: Effect<R, E, A>,
+      f: X
+    ): [X] extends [Effect<infer R1, infer E1, infer _A1>] ? Effect<R | R1, E | E1, A>
+      : [X] extends [Promise<infer _A1>] ? Effect<R, Cause.UnknownException | E, A>
+      : Effect<R, E, A>
+  }
 }
