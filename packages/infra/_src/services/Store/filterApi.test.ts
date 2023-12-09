6f93363a70b40ce6636cdbbbc21480dfadab4914
Patrick Roza <contact@patrickroza.com>
2023-12-09 14:41:45 +0100

    more api

--- packages/infra/_src/services/Store/filterApi.test.ts
+++ packages/infra/_src/services/Store/filterApi.test.ts
@@ -2,27 +2,42 @@ import type { FieldValues } from "../../filter/types.js"
 import type { FieldPath, FieldPathValue } from "../../filter/types/path/eager.js"
 
 const FilterBuilder = {
-  make: <T extends FieldValues>() => {
-    return null as unknown as FilterBuilder<T>
+  make: <TFieldValues extends FieldValues>(): Initial<TFieldValues> => {
+    throw new Error("Not implemented")
   }
 }
 
-type Filter<TFieldValues extends FieldValues> = <
-  TFieldName extends FieldPath<TFieldValues>,
-  V extends FieldPathValue<TFieldValues, TFieldName>
->(
-  path: TFieldName,
-  value: V
+type Initial<TFieldValues extends FieldValues> = { where: Filter<TFieldValues> }
+
+type Filter<TFieldValues extends FieldValues> = {
+  (
+    fb: (f: Initial<TFieldValues>) => FilterBuilder<TFieldValues>
+  ): FilterBuilder<TFieldValues>
+  <
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    value: V
+  ): FilterBuilder<TFieldValues>
+  <
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    t: "in",
+    value: readonly V[]
+  ): FilterBuilder<TFieldValues>
+}
+
+type FilterGroup<TFieldValues extends FieldValues> = (
+  fb: (f: FilterBuilder<TFieldValues>) => FilterBuilder<TFieldValues>
 ) => FilterBuilder<TFieldValues>
 
 interface FilterBuilder<TFieldValues extends FieldValues> {
   // TODO: as overloads?
-  andGroup: (fb: (f: FilterBuilder<TFieldValues>) => FilterBuilder<TFieldValues>) => FilterBuilder<TFieldValues>
-  orGroup: (fb: (f: FilterBuilder<TFieldValues>) => FilterBuilder<TFieldValues>) => FilterBuilder<TFieldValues>
   and: Filter<TFieldValues>
   or: Filter<TFieldValues>
-
-  where: Filter<TFieldValues>
   // where: <Filters extends Record<TFieldName extends FieldPath<TFieldValues>, V extends Value< FieldPathValue<TFieldValues, TFieldName>>>(filter: Filters) => FilterBuilder<TFieldValues>
 }
 
@@ -40,10 +55,14 @@ interface MyEntity {
 }
 
 it("works", () => {
-  const filter = FilterBuilder.make<MyEntity>()
-  filter
+  FilterBuilder
+    .make<MyEntity>()
     .where("something.id", 1)
-    .andGroup((_) => _.where("something.name", "a"))
+    .and((_) =>
+      _
+        .where("something.name", "a")
+        .or("name", "in", ["a", "b"])
+    )
     .and("isActive", true)
     .and("age", 12)
 })
