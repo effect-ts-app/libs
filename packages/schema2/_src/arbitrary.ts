cc829e5d0339a958567c8c4dde7a86e875bd5d09
Patrick Roza <contact@patrickroza.com>
2023-12-24 12:29:30 +0100

    more strings and Arbitrary.

---
+++ packages/schema2/_src/arbitrary.ts
@@ -0,0 +1,17 @@
+import type { Arbitrary } from "@effect/schema/Arbitrary"
+import * as fc from "fast-check"
+import { A } from "./schema.js"
+import type { S } from "./schema.js"
+
+/**
+ * @tsplus getter effect/schema/Arbitrary generate
+ * @tsplus getter effect/schema/Arbitrary sample
+ */
+export function sampleFromArb<T>(arb: Arbitrary<T>) {
+  return fc.sample(arb(fc), 1)[0] // TODO: whats the difference with generate?
+}
+
+/**
+ * @tsplus getter effect/schema/Schema Arbitrary
+ */
+export const Arb = <From, To>(s: S.Schema<From, To>): Arbitrary<To> => A.to(s)
