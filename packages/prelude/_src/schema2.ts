964d3aaca84ebd85b34576a4b5bab60867ab24a9
Patrick Roza <contact@patrickroza.com>
2023-12-24 17:05:04 +0100

    prep

---
+++ packages/prelude/_src/schema2.ts
@@ -0,0 +1,20 @@
+// // codegen:start {preset: barrel, include: ./schema/*, exclude: [./schema/events.ts, ./schema/overrides.ts]}
+// export * from "./schema/_schema.js"
+// export * from "./schema/api.js"
+// export * from "./schema/shared.js"
+// // codegen:end
+
+// export { Email, PhoneNumber } from "./schema/shared.js"
+export * from "@effect-app/schema2"
+export * from "@effect-app/schema2/email"
+export * from "@effect-app/schema2/moreStrings"
+export * from "@effect-app/schema2/phoneNumber"
+export * from "@effect-app/schema2/strings"
+
+/**
+ * Allow anonymous access
+ */
+export function allowAnonymous(cls: any) {
+  Object.assign(cls, { allowAnonymous: true })
+  return cls
+}
