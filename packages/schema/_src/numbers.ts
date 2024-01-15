fd85bed014db5ce7fc9a0e2ea6dba04b0e769c77
Patrick Roza <contact@patrickroza.com>
2024-01-15 09:30:40 +0100

    Int inheritance

--- packages/schema/_src/numbers.ts
+++ packages/schema/_src/numbers.ts
@@ -2,12 +2,14 @@ import type { Simplify } from "effect/Types"
 import { fromBrand, nominal } from "./ext.js"
 import { type B, S } from "./schema.js"
 
-export interface PositiveIntBrand extends Simplify<B.Brand<"PositiveInt"> & NonNegativeIntBrand> {}
+export interface PositiveIntBrand
+  extends Simplify<B.Brand<"PositiveInt"> & NonNegativeIntBrand & PositiveNumberBrand>
+{}
 export const PositiveInt =
   S.Int.pipe(S.positive(), fromBrand(nominal<PositiveIntBrand>(), { jsonSchema: {} })).withDefaults
 export type PositiveInt = S.Schema.To<typeof PositiveInt>
 
-export interface NonNegativeIntBrand extends Simplify<B.Brand<"NonNegativeInt"> & IntBrand> {}
+export interface NonNegativeIntBrand extends Simplify<B.Brand<"NonNegativeInt"> & IntBrand & NonNegativeNumberBrand> {}
 export const NonNegativeInt =
   S.Int.pipe(S.nonNegative(), fromBrand(nominal<NonNegativeIntBrand>(), { jsonSchema: {} })).withDefaults
 export type NonNegativeInt = S.Schema.To<typeof NonNegativeInt>
