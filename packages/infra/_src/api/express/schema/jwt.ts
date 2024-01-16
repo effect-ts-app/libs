067274a96be81ccea6cae79bb2ce7f8b939321a0
Patrick Roza <contact@patrickroza.com>
2024-01-16 12:49:52 +0100

    housekeeping: update packages

--- packages/infra/_src/api/express/schema/jwt.ts
+++ packages/infra/_src/api/express/schema/jwt.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
 import * as S from "@effect-app/schema"
+import type { ParseError } from "@effect/schema/ParseResult"
 
 import { jwtDecode } from "jwt-decode"
 
@@ -13,7 +14,7 @@ export const jwt = S.transformOrFail(
       try: () => jwtDecode(s),
       catch: (e: any) => ParseResult.parseError(ParseResult.type(ast, s, e.message))
     }),
-  (_): ParseResult.ParseResult<string> => {
+  (_): Effect<never, ParseError, string> => {
     throw new Error("not implemented")
   }
 )
