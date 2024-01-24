861472700c540f01a97805ca858515fee959fcf0
Patrick Roza <contact@patrickroza.com>
2024-01-24 22:07:46 +0100

    `parseJwt`

--- packages/infra/_src/api/express/schema/jwt.ts
+++ packages/infra/_src/api/express/schema/jwt.ts
@@ -6,15 +6,18 @@ import type { ParseIssue } from "@effect/schema/ParseResult"
 
 import { jwtDecode } from "jwt-decode"
 
-export const jwt = S.transformOrFail(
-  S.string,
-  S.unknown,
-  (s, __, ast) =>
-    ParseResult.try({
-      try: () => jwtDecode(s),
-      catch: (e: any) => ParseResult.type(ast, s, e.message)
-    }),
-  (_): Effect<never, ParseIssue, string> => {
-    throw new Error("not implemented")
-  }
-)
+export const parseJwt = <R, I, A>(schema: Schema<R, I, A>): Schema<R, string, A> =>
+  S
+    .transformOrFail(
+      S.string,
+      S.unknown,
+      (s, __, ast) =>
+        ParseResult.try({
+          try: () => jwtDecode(s),
+          catch: (e: any) => ParseResult.type(ast, s, e?.message)
+        }),
+      (_): Effect<never, ParseIssue, string> => {
+        throw new Error("not implemented")
+      }
+    )
+    .compose(schema as Schema<R, unknown, A>)
