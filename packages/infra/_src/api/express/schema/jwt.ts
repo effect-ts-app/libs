b3ef7608a6cf14ed329309060ada42c24a272d05
Patrick Roza <contact@patrickroza.com>
2024-01-25 08:36:52 +0100

    improve: jwt

--- packages/infra/_src/api/express/schema/jwt.ts
+++ packages/infra/_src/api/express/schema/jwt.ts
@@ -3,21 +3,23 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import * as S from "@effect-app/schema"
 import type { ParseIssue } from "@effect/schema/ParseResult"
+import { jwtDecode, type JwtDecodeOptions } from "jwt-decode"
 
-import { jwtDecode } from "jwt-decode"
-
-export const parseJwt = <R, I, A>(schema: Schema<R, I, A>): Schema<R, string, A> =>
+export const parseJwt = <R, I, A>(
+  schema: Schema<R, I, A>,
+  options?: JwtDecodeOptions | undefined
+): Schema<R, string, A> =>
   S
     .transformOrFail(
       S.string,
       S.unknown,
       (s, __, ast) =>
         ParseResult.try({
-          try: () => jwtDecode(s),
+          try: () => jwtDecode(s, options),
           catch: (e: any) => ParseResult.type(ast, s, e?.message)
         }),
       (_): Effect<never, ParseIssue, string> => {
         throw new Error("not implemented")
       }
     )
-    .compose(schema as Schema<R, unknown, A>)
+    .compose(schema, { strict: false })
