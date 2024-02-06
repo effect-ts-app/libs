5661292c8f1455dea539814d959830c83acb08f1
Patrick Roza <contact@patrickroza.com>
2024-02-06 10:17:02 +0100

    order

--- packages/fluent-extensions/_src/index.ts
+++ packages/fluent-extensions/_src/index.ts
@@ -13,6 +13,66 @@ import "effect/fluentExtensions"
 
 export {}
 
+declare module "effect/Effect" {
+  export interface Effect<R, E, A> {
+    andThen<A, R, E, X>(
+      this: Effect<R, E, A>,
+      f: (a: NoInfer<A>) => X
+    ): [X] extends [Effect<infer R1, infer E1, infer A1>] ? Effect<R | R1, E | E1, A1>
+      : [X] extends [Promise<infer A1>] ? Effect<R, Cause.UnknownException | E, A1>
+      : Effect<R, E, X>
+
+    andThen<A, R, E, X>(
+      this: Effect<R, E, A>,
+      f: X
+    ): [X] extends [Effect<infer R1, infer E1, infer A1>] ? Effect<R | R1, E | E1, A1>
+      : [X] extends [Promise<infer A1>] ? Effect<R, Cause.UnknownException | E, A1>
+      : Effect<R, E, X>
+    tap<A, R, E, X>(
+      this: Effect<R, E, A>,
+      f: (a: NoInfer<A>) => X
+    ): [X] extends [Effect<infer R1, infer E1, infer _A1>] ? Effect<R | R1, E | E1, A>
+      : [X] extends [Promise<infer _A1>] ? Effect<R, Cause.UnknownException | E, A>
+      : Effect<R, E, A>
+    tap<A, R, E, X>(
+      this: Effect<R, E, A>,
+      f: X
+    ): [X] extends [Effect<infer R1, infer E1, infer _A1>] ? Effect<R | R1, E | E1, A>
+      : [X] extends [Promise<infer _A1>] ? Effect<R, Cause.UnknownException | E, A>
+      : Effect<R, E, A>
+  }
+}
+
+declare module "effect/Cause" {
+  export interface YieldableError {
+    andThen<A, R, E, X>(
+      this: Effect.Effect<R, E, A>,
+      f: (a: NoInfer<A>) => X
+    ): [X] extends [Effect.Effect<infer R1, infer E1, infer A1>] ? Effect.Effect<R | R1, E | E1, A1>
+      : [X] extends [Promise<infer A1>] ? Effect.Effect<R, UnknownException | E, A1>
+      : Effect.Effect<R, E, X>
+
+    andThen<A, R, E, X>(
+      this: Effect.Effect<R, E, A>,
+      f: X
+    ): [X] extends [Effect.Effect<infer R1, infer E1, infer A1>] ? Effect.Effect<R | R1, E | E1, A1>
+      : [X] extends [Promise<infer A1>] ? Effect.Effect<R, UnknownException | E, A1>
+      : Effect.Effect<R, E, X>
+    tap<A, R, E, X>(
+      this: Effect.Effect<R, E, A>,
+      f: (a: NoInfer<A>) => X
+    ): [X] extends [Effect.Effect<infer R1, infer E1, infer _A1>] ? Effect.Effect<R | R1, E | E1, A>
+      : [X] extends [Promise<infer _A1>] ? Effect.Effect<R, UnknownException | E, A>
+      : Effect.Effect<R, E, A>
+    tap<A, R, E, X>(
+      this: Effect.Effect<R, E, A>,
+      f: X
+    ): [X] extends [Effect.Effect<infer R1, infer E1, infer _A1>] ? Effect.Effect<R | R1, E | E1, A>
+      : [X] extends [Promise<infer _A1>] ? Effect.Effect<R, UnknownException | E, A>
+      : Effect.Effect<R, E, A>
+  }
+}
+
 declare module "effect/Option" {
   export interface None<out A> {
     get value(): A | undefined
@@ -669,65 +729,5 @@ declare global {
         readonly discard: true
       }
     ): Effect.Effect<R, E, void>
-  }
-}
-
-declare module "effect/Cause" {
-  export interface YieldableError {
-    andThen<A, R, E, X>(
-      this: Effect.Effect<R, E, A>,
-      f: (a: NoInfer<A>) => X
-    ): [X] extends [Effect.Effect<infer R1, infer E1, infer A1>] ? Effect.Effect<R | R1, E | E1, A1>
-      : [X] extends [Promise<infer A1>] ? Effect.Effect<R, UnknownException | E, A1>
-      : Effect.Effect<R, E, X>
-
-    andThen<A, R, E, X>(
-      this: Effect.Effect<R, E, A>,
-      f: X
-    ): [X] extends [Effect.Effect<infer R1, infer E1, infer A1>] ? Effect.Effect<R | R1, E | E1, A1>
-      : [X] extends [Promise<infer A1>] ? Effect.Effect<R, UnknownException | E, A1>
-      : Effect.Effect<R, E, X>
-    tap<A, R, E, X>(
-      this: Effect.Effect<R, E, A>,
-      f: (a: NoInfer<A>) => X
-    ): [X] extends [Effect.Effect<infer R1, infer E1, infer _A1>] ? Effect.Effect<R | R1, E | E1, A>
-      : [X] extends [Promise<infer _A1>] ? Effect.Effect<R, UnknownException | E, A>
-      : Effect.Effect<R, E, A>
-    tap<A, R, E, X>(
-      this: Effect.Effect<R, E, A>,
-      f: X
-    ): [X] extends [Effect.Effect<infer R1, infer E1, infer _A1>] ? Effect.Effect<R | R1, E | E1, A>
-      : [X] extends [Promise<infer _A1>] ? Effect.Effect<R, UnknownException | E, A>
-      : Effect.Effect<R, E, A>
-  }
-}
-
-declare module "effect/Effect" {
-  export interface Effect<R, E, A> {
-    andThen<A, R, E, X>(
-      this: Effect<R, E, A>,
-      f: (a: NoInfer<A>) => X
-    ): [X] extends [Effect<infer R1, infer E1, infer A1>] ? Effect<R | R1, E | E1, A1>
-      : [X] extends [Promise<infer A1>] ? Effect<R, Cause.UnknownException | E, A1>
-      : Effect<R, E, X>
-
-    andThen<A, R, E, X>(
-      this: Effect<R, E, A>,
-      f: X
-    ): [X] extends [Effect<infer R1, infer E1, infer A1>] ? Effect<R | R1, E | E1, A1>
-      : [X] extends [Promise<infer A1>] ? Effect<R, Cause.UnknownException | E, A1>
-      : Effect<R, E, X>
-    tap<A, R, E, X>(
-      this: Effect<R, E, A>,
-      f: (a: NoInfer<A>) => X
-    ): [X] extends [Effect<infer R1, infer E1, infer _A1>] ? Effect<R | R1, E | E1, A>
-      : [X] extends [Promise<infer _A1>] ? Effect<R, Cause.UnknownException | E, A>
-      : Effect<R, E, A>
-    tap<A, R, E, X>(
-      this: Effect<R, E, A>,
-      f: X
-    ): [X] extends [Effect<infer R1, infer E1, infer _A1>] ? Effect<R | R1, E | E1, A>
-      : [X] extends [Promise<infer _A1>] ? Effect<R, Cause.UnknownException | E, A>
-      : Effect<R, E, A>
   }
 }
