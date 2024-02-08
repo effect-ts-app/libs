c3e7fae81fb4819e266e07abbb30dca80ceb3d5c
Patrick Roza <contact@patrickroza.com>
2024-02-08 16:27:23 +0100

    updates

--- scripts/convert_patch.js
+++ scripts/convert_patch.js
@@ -5,7 +5,10 @@ fs.readdirSync("./patches").filter(_ => _.slice(1).includes("@")).forEach((f) =>
   let [packageName_, maybeVersion, version] = f.replace(".patch", "").split("@")
   const packageName = (version ? ("@" + maybeVersion) : packageName_).split("__").join("/")
   version = version ?? maybeVersion
-  console.log({packageName, version})
-  const content = fs.readFileSync(`./patches/${f}`, "utf-8").replaceAll(" a/", ` a/node_modules/${packageName}/`).replaceAll(" b/", ` b/node_modules/${packageName}/`)
-  fs.writeFileSync(`./patches/${packageName.replace("/", "+")}+${version}.patch`, content)
+  const oldPatchFile = `./patches/${f}`
+  const content = fs.readFileSync(oldPatchFile, "utf-8").replaceAll(" a/", ` a/node_modules/${packageName}/`).replaceAll(" b/", ` b/node_modules/${packageName}/`)
+  const newPatchFile = `./patches/${packageName.replace("/", "+")}+${version}.patch`
+  fs.writeFileSync(newPatchFile, content)
+
+  fs.rmSync(oldPatchFile)
 })
