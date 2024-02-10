0d02cbdf7e83db52a07fbff8c51ba3e43876b89d
Patrick Roza <contact@patrickroza.com>
2024-02-10 15:52:39 +0100

    fix indexjs

---
+++ packages/core/_src/Prelude.code.ts
@@ -0,0 +1,18 @@
+// TODO: Export order seems to be important? Somehow some cyclics?
+export * as Effect from "@effect-app/core/Effect"
+export * as Optic from "@fp-ts/optic"
+export * as Chunk from "effect/Chunk"
+export * as Either from "effect/Either"
+export * as Option from "effect/Option"
+
+export * as Order from "effect/Order"
+
+export {} from "@effect-app/core/Effect"
+// export * as EffectOption from "@effect-app/core/EffectOption"
+export * as NonEmptySet from "@effect-app/core/NonEmptySet"
+export {} from "@effect-app/core/Option"
+// export * as Schema from "@effect-app/prelude/schema"
+export * as Set from "@effect-app/core/Set"
+export * as ReadonlySet from "@effect-app/core/Set"
+
+export * as ReadonlyArray from "@effect-app/core/Array"
