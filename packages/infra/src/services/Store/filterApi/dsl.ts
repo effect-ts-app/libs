dfa0539a0ca3215d9bbe66b85ffdea648a117464
Patrick Roza <contact@patrickroza.com>
2024-02-22 10:38:39 +0100

    progress

--- packages/infra/src/services/Store/filterApi/dsl.ts
+++ packages/infra/src/services/Store/filterApi/dsl.ts
@@ -1,5 +1,7 @@
 import type { FieldValues } from "../../../filter/types.js"
-import type { QueryBuilder } from "./query.js"
+import type { FieldPath, FieldPathValue } from "../../../filter/types/path/eager.js"
+import type { Ops } from "./proxy.js"
+import type { Initial, QueryBuilder } from "./query.js"
 
 export interface Query<TFieldValues extends FieldValues> {
   readonly _id: unique symbol
@@ -8,23 +10,168 @@ export interface QueryWhere<TFieldValues extends FieldValues> extends Query<TFie
   readonly _id2: unique symbol
 }
 
+// Impl, later
 // export type All<TFieldValues extends FieldValues> = Value<TFieldValues> | Where<TFieldValues>
 
 // export class Value<TFieldValues extends FieldValues> extends Data.TaggedClass("value")<{ value: TFieldValues }> {}
 
 // export class Where<TFieldValues extends FieldValues> extends Data.TaggedClass("where")<{}> {}
 
-export declare const where: <TFieldValues extends FieldValues>() => (
-  current: Query<TFieldValues>
-) => QueryWhere<TFieldValues>
+export declare const where: FilterWhere
 
-export declare const and: <TFieldValues extends FieldValues>() => (
-  current: Query<TFieldValues>
-) => QueryWhere<TFieldValues>
+export declare const and: FilterContinuation
 
-export declare const or: <TFieldValues extends FieldValues>() => (
-  current: Query<TFieldValues>
-) => QueryWhere<TFieldValues>
+export declare const or: FilterContinuation
+
+// TODO: able to switch to Order and Limit/Take, which are "enders"
+export type FilterWheres = {
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    value: V
+  ): (
+    current: Query<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    op: "neq",
+    value: V
+  ): (
+    current: Query<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    op: "gt" | "gte" | "lt" | "lte",
+    value: V // only numbers?
+  ): (
+    current: Query<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    op: "startsWith" | "endsWith" | "contains" | "notContains" | "notStartsWith" | "notEndsWith",
+    value: V // only strings?
+  ): (
+    current: Query<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    op: "in" | "notIn",
+    value: readonly V[]
+  ): (
+    current: Query<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+}
+
+export type FilterWhere = {
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(f: {
+    path: TFieldName
+    op: Ops
+    value: V
+  }): (
+    current: Query<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+} & FilterWheres
+
+export type FilterContinuations = {
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    value: V
+  ): (
+    current: QueryWhere<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    op: "neq",
+    value: V
+  ): (
+    current: QueryWhere<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    op: "gt" | "gte" | "lt" | "lte",
+    value: V // only numbers?
+  ): (
+    current: QueryWhere<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    op: "startsWith" | "endsWith" | "contains" | "notContains" | "notStartsWith" | "notEndsWith",
+    value: V // only strings?
+  ): (
+    current: QueryWhere<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(
+    path: TFieldName,
+    op: "in" | "notIn",
+    value: readonly V[]
+  ): (
+    current: QueryWhere<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+}
+
+export type FilterContinuation = {
+  <TFieldValues extends FieldValues>(
+    fb: (f: FilterContinuations & Initial<TFieldValues>) => (
+      current: QueryWhere<TFieldValues>
+    ) => QueryWhere<TFieldValues>
+  ): (
+    current: QueryWhere<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+  <
+    TFieldValues extends FieldValues,
+    TFieldName extends FieldPath<TFieldValues>,
+    V extends FieldPathValue<TFieldValues, TFieldName>
+  >(f: {
+    path: TFieldName
+    op: Ops
+    value: V
+  }): (
+    current: QueryWhere<TFieldValues>
+  ) => QueryWhere<TFieldValues>
+} & FilterContinuations
 
 export declare const value: <TFieldValues extends FieldValues>() => Query<TFieldValues>
 
@@ -35,9 +182,16 @@ export interface A {
 }
 
 declare const filter: ReturnType<typeof QueryBuilder.make<A>>
-filter((where) => where())
+filter((where) =>
+  where("a", "in", ["a"])
+    .and("b", "gt", 1)
+    .or((where) => where("a", "b")) // able to create scope
+)
 
 const p = pipe(
   value<A>(),
-  where()
+  where("b", "neq", 1),
+  and("b", "gt", 1),
+  and("a", "neq", "b"),
+  or(() => where("b", 2))
 )
