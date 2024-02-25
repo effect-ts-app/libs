f7a51d987798a157ffb7a92debf15a9ea0f242b0
Patrick Roza <contact@patrickroza.com>
2024-02-25 20:24:33 +0100

    progress

--- packages/infra/src/api/routing/defaultErrorHandler.ts
+++ packages/infra/src/api/routing/defaultErrorHandler.ts
@@ -1,6 +1,7 @@
 import { logError } from "@effect-app/infra/errorReporter"
 import type { Schema } from "@effect-app/schema"
-import { Data, Effect, Schedule } from "effect-app"
+import { setBody, setStatus } from "@effect/platform/Http/ServerResponse"
+import { Cause, Data, Effect, S, Schedule } from "effect-app"
 import type { SupportedErrors } from "effect-app/client/errors"
 import {
   InvalidStateError,
@@ -10,13 +11,13 @@ import {
   UnauthorizedError,
   ValidationError
 } from "effect-app/client/errors"
+import { HttpBody, HttpHeaders, type HttpServerRequest, HttpServerResponse } from "effect-app/http"
 import type {
   InsufficientScopeError,
   InvalidRequestError,
   InvalidTokenError,
   UnauthorizedError as JWTUnauthorizedError
 } from "express-oauth2-jwt-bearer"
-import { HttpBody, HttpHeaders, type HttpServerRequest, type HttpServerResponse } from "../http.js"
 
 export class JWTError extends Data.TaggedClass("JWTError")<{
   error:
@@ -29,73 +30,81 @@ export class JWTError extends Data.TaggedClass("JWTError")<{
 const logRequestError = logError("Request")
 
 export function defaultBasicErrorHandler<R>(
-  _req: HttpServerRequest,
-  res: HttpServerResponse,
-  r2: Effect<HttpServerResponse, ValidationError, R>
+  _req: HttpServerRequest.ServerRequest,
+  res: HttpServerResponse.ServerResponse,
+  r2: Effect<HttpServerResponse.ServerResponse, ValidationError, R>
 ) {
   const sendError = (code: number) => (body: unknown) =>
-    Effect.sync(() => res.setBody(HttpBody.unsafeJson(body)).setStatus(code))
-  return r2
-    .tapErrorCause((cause) => cause.isFailure() ? logRequestError(cause) : Effect.unit)
-    .catchTag("ValidationError", (err) => sendError(400)(err.errors))
-    // final catch all; expecting never so that unhandled known errors will show up
-    .catchAll((err: never) =>
-      Effect
-        .logError(
-          "Program error, compiler probably silenced, got an unsupported Error in Error Channel of Effect" + err
-        )
-        .map(() => err as unknown)
-        .flatMap(Effect.die)
-    )
+    Effect.sync(() => setBody(res, HttpBody.unsafeJson(body)).pipe(setStatus(code)))
+  return r2.pipe(
+    Effect.tapErrorCause((cause) => Cause.isFailure(cause) ? logRequestError(cause) : Effect.unit),
+    Effect.catchTag("ValidationError", (err) => sendError(400)(err.errors)),
+    Effect
+      // final catch all; expecting never so that unhandled known errors will show up
+      .catchAll((err: never) =>
+        Effect
+          .logError(
+            "Program error, compiler probably silenced, got an unsupported Error in Error Channel of Effect" + err
+          )
+          .pipe(
+            Effect.map(() => err as unknown),
+            Effect.flatMap(Effect.die)
+          )
+      )
+  )
 }
 
 const optimisticConcurrencySchedule = Schedule.once
   && Schedule.recurWhile<SupportedErrors | JWTError>((a) => a._tag === "OptimisticConcurrencyException")
 
 export function defaultErrorHandler<R>(
-  req: HttpServerRequest,
-  res: HttpServerResponse,
-  r2: Effect<HttpServerResponse, SupportedErrors | JWTError, R>
+  req: HttpServerRequest.ServerRequest,
+  res: HttpServerResponse.ServerResponse,
+  r2: Effect<HttpServerResponse.ServerResponse, SupportedErrors | JWTError, R>
 ) {
   const r3 = req.method === "PATCH"
-    ? r2.retry(optimisticConcurrencySchedule)
+    ? Effect.retry(r2, optimisticConcurrencySchedule)
     : r2
   const sendError = <R, From, To>(code: number, schema: Schema<To, From, R>) => (body: To) =>
-    schema
-      .encode(body)
-      .orDie
-      .andThen((body) =>
-        res
-          .setStatus(code)
-          .setBody(HttpBody.unsafeJson(body))
+    S
+      .encode(schema)(body)
+      .pipe(
+        Effect.orDie,
+        Effect.andThen((body) => res.pipe(setStatus(code), setBody(HttpBody.unsafeJson(body))))
       )
   return r3
-    .tapErrorCause((cause) => cause.isFailure() ? logRequestError(cause) : Effect.unit)
-    .catchTags({
-      "JWTError": (err) =>
-        Effect.succeed(
-          HttpServerResponse.unsafeJson({ message: err.error.message }, {
-            status: err
-              .error
-              .status,
-            headers: HttpHeaders.fromInput(err.error.headers)
-          })
-        ),
-      "ValidationError": sendError(400, ValidationError),
-      "NotFoundError": sendError(404, NotFoundError),
-      "NotLoggedInError": sendError(401, NotLoggedInError),
-      "UnauthorizedError": sendError(403, UnauthorizedError),
-      "InvalidStateError": sendError(422, InvalidStateError),
-      // 412 or 409.. https://stackoverflow.com/questions/19122088/which-http-status-code-to-use-to-reject-a-put-due-to-optimistic-locking-failure
-      "OptimisticConcurrencyException": sendError(412, OptimisticConcurrencyException)
-    })
-    // final catch all; expecting never so that unhandled known errors will show up
-    .catchAll((err: never) =>
+    .pipe(
+      Effect.tapErrorCause((cause) => Cause.isFailure(cause) ? logRequestError(cause) : Effect.unit),
       Effect
-        .logError(
-          "Program error, compiler probably silenced, got an unsupported Error in Error Channel of Effect" + err
+        .catchTags({
+          "JWTError": (err) =>
+            Effect.succeed(
+              HttpServerResponse.unsafeJson({ message: err.error.message }, {
+                status: err
+                  .error
+                  .status,
+                headers: HttpHeaders.fromInput(err.error.headers)
+              })
+            ),
+          "ValidationError": sendError(400, ValidationError),
+          "NotFoundError": sendError(404, NotFoundError),
+          "NotLoggedInError": sendError(401, NotLoggedInError),
+          "UnauthorizedError": sendError(403, UnauthorizedError),
+          "InvalidStateError": sendError(422, InvalidStateError),
+          // 412 or 409.. https://stackoverflow.com/questions/19122088/which-http-status-code-to-use-to-reject-a-put-due-to-optimistic-locking-failure
+          "OptimisticConcurrencyException": sendError(412, OptimisticConcurrencyException)
+        }),
+      Effect
+        // final catch all; expecting never so that unhandled known errors will show up
+        .catchAll((err: never) =>
+          Effect
+            .logError(
+              "Program error, compiler probably silenced, got an unsupported Error in Error Channel of Effect" + err
+            )
+            .pipe(
+              Effect.map(() => err as unknown),
+              Effect.flatMap(Effect.die)
+            )
         )
-        .map(() => err as unknown)
-        .flatMap(Effect.die)
     )
 }
