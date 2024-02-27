ec1070c224d82d93de61098dce89116992b87efe
Patrick Roza <contact@patrickroza.com>
2024-02-27 12:53:03 +0100

    veet

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -1,26 +1,23 @@
 /// <reference types="vitest" />
-import { effectPlugin } from "@effect-app/compiler/vitePlugin2"
 import path from "path"
 import fs from "fs"
 
-const useDist = process.env.TEST_USE_DIST === "true"
 
 export default function makeConfig(dirName?: string) {
+  const prefix = path.resolve(__dirname, "packages")
+  const packages = fs.readdirSync(prefix).map(f => prefix + "/" + f).filter(f => fs.lstatSync(f).isDirectory() )
   return {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    plugins: useDist ? [] : [effectPlugin({})],
+    plugins: [],
     test: {
-      include: useDist ? ["./dist/**/*.test.js"] : ["./src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+      include:  ["./src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
       exclude: ["./_test/**/*"],
       reporters: "verbose",
-      globals: true,
-      deps: useDist ? { inline: [new RegExp(dirName + "/dist")], } : undefined,
+      globals: true
     },
-    resolve: dirName
-      ? {
-        alias: {
-          [JSON.parse(fs.readFileSync(dirName + "/package.json", "utf-8")).name]: path.resolve(dirName, useDist ? "/dist" : "/src")
-        } }
-      : undefined,
+    resolve: packages.reduce((acc, cur) => {
+      acc[JSON.parse(fs.readFileSync(cur + "/package.json", "utf-8")).name] = path.resolve(cur, "/src")
+      return acc
+    }, {})
   }
 }
