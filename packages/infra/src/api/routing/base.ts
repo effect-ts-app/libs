c75dc699e53365265fee41b6ebc337a44919b6c5
Patrick Roza <contact@patrickroza.com>
2024-03-11 20:51:00 +0100

    cleanup

--- packages/infra/src/api/routing/base.ts
+++ packages/infra/src/api/routing/base.ts
@@ -11,34 +11,6 @@ import type { HttpRouter, HttpServerError } from "effect-app/http"
 import type { REST, StructFields } from "effect-app/schema"
 import type { Simplify } from "effect/Types"
 
-export type Flatten<T extends object> = object extends T ? object : {
-  [K in keyof T]-?: (
-    x: NonNullable<T[K]> extends infer V ? V extends object ? V extends readonly any[] ? Pick<T, K>
-        : FlattenLVL1<V> extends infer FV ? ({
-            [P in keyof FV as `${Extract<K, string | number>}.${Extract<P, string | number>}`]: FV[P]
-          })
-        : never
-      : Pick<T, K>
-      : never
-  ) => void
-} extends Record<keyof T, (y: infer O) => void> ? O extends unknown /* infer U */ ? { [K in keyof O]: O[K] } : never
-: never
-
-type FlattenLVL1<T extends object> = object extends T ? object : {
-  [K in keyof T]-?: (
-    x: NonNullable<T[K]> extends infer V ? V extends object ? V extends readonly any[] ? Pick<T, K>
-          /*: Flatten<V> extends infer FV ? ({
-      [P in keyof FV as `${Extract<K, string | number>}.${Extract<P, string | number>}`]: FV[P]
-    })
-    : never
-    */
-        : Pick<T, K>
-      : never
-      : never
-  ) => void
-} extends Record<keyof T, (y: infer O) => void> ? O extends unknown /* infer U */ ? { [K in keyof O]: O[K] } : never
-: never
-
 export type RouteMatch<
   R,
   M,
