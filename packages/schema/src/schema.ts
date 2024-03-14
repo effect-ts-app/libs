875850e78b231e4eb8d0e940b101b84261293977
Patrick Roza <contact@patrickroza.com>
2024-03-14 09:26:02 +0100

    fix import

--- packages/schema/src/schema.ts
+++ packages/schema/src/schema.ts
@@ -1,6 +1,6 @@
 import * as A from "@effect/schema/Arbitrary"
 import * as AST from "@effect/schema/AST"
-import * as P from "@effect/schema/Parser"
+import * as P from "@effect/schema/ParseResult"
 import type * as B from "effect/Brand"
 
 export { A, AST, B, P }
