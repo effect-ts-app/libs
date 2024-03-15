aef497ec5a4ff0c40224565d1a248f34eef6f716
Patrick Roza <contact@patrickroza.com>
2024-03-15 21:16:28 +0100

    improve flow/pipe inside scope

--- packages/infra/test/query.test.ts
+++ packages/infra/test/query.test.ts
@@ -1,4 +1,4 @@
-import { Effect, flow, Layer, Option, pipe, S } from "effect-app"
+import { Effect, flow, Layer, Option, S } from "effect-app"
 import { TagClassMakeId } from "effect-app/service"
 import { pick } from "effect-app/utils"
 import { inspect } from "util"
@@ -19,30 +19,41 @@ export class s extends S.Class<s>()({
   union: someUnion.pipe(S.withDefaultConstructor(() => ({ _tag: "string" as const, value: "hi" })))
 }) {}
 export declare namespace s {
-  export type From = S.Schema.Encoded<typeof s>
+  export interface From extends S.Schema.Encoded<typeof s> {}
 }
 
 const MakeSomeService = Effect.succeed({ a: 1 })
 export class SomeService extends TagClassMakeId("SomeService", MakeSomeService)<SomeService>() {}
 
-const q = pipe(
-  make<s.From>(), // provided automatically inside Repo.q2()
-  where("displayName", "Verona"),
-  or(flow(
-    where("displayName", "Riley"),
-    and("n", "gt", "2021-01-01T00:00:00Z") // TODO: work with To type translation, so Date?
-  )),
-  order("displayName"),
-  page({ take: 10 }),
-  project(
-    S.transformOrFail(
-      S.struct({ id: S.StringId, displayName: S.string }), // for projection performance benefit, this should be limited to the fields interested, and leads to SELECT fields
-      S.struct(pick(s.fields, "id", "displayName")),
-      (_) => Effect.andThen(SomeService, _),
-      () => Effect.die(new Error("not implemented"))
-    )
+const q = make<s.From>()
+  .pipe( // provided automatically inside Repo.q2()
+    where("displayName", "Verona"),
+    or(
+      where("displayName", "Riley"),
+      and("displayName", "Riley"),
+      and("displayName", "Riley"),
+      and("displayName", "Riley"),
+      and("displayName", "Riley"),
+      or("union._tag", "number"),
+      and("displayName", "Riley"),
+      and("displayName", "Riley"),
+      and("displayName", "Riley"),
+      and("displayName", "Riley"),
+      or("displayName", "Riley"),
+      and("displayName", "Riley")
+      // and("n", "gt", "2021-01-01T00:00:00Z") // TODO: work with To type translation, so Date?
+    ),
+    order("displayName"),
+    page({ take: 10 })
+    // project(
+    //   S.transformOrFail(
+    //     S.struct({ id: S.StringId, displayName: S.string }), // for projection performance benefit, this should be limited to the fields interested, and leads to SELECT fields
+    //     S.struct(pick(s.fields, "id", "displayName")),
+    //     (_) => Effect.andThen(SomeService, _),
+    //     () => Effect.die(new Error("not implemented"))
+    //   )
+    // )
   )
-)
 
 const items = [
   new s({ displayName: S.NonEmptyString255("Verona"), n: new Date("2020-01-01T00:00:00Z") }),
