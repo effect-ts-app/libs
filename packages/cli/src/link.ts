8dc2b6246d36d0e66a4e89603e56957f07ef48e2
Patrick Roza <contact@patrickroza.com>
2024-03-28 09:24:22 +0100

    improve link

--- packages/cli/src/link.ts
+++ packages/cli/src/link.ts
@@ -1,18 +1,20 @@
 import cp from "child_process"
 import fs from "fs"
+import { EFFECT_APP_LIBS, packages } from "./shared.js"
 
 const pj = (await import(process.cwd() + "/package.json", { assert: { type: "json" } })).default
 
 pj.resolutions = {
   ...pj.resolutions,
-  "@effect-app/core": "file:../../effect-app/libs/packages/core",
-  "@effect-app/eslint-codegen-model": "file:../../effect-app/libs/packages/eslint-codegen-model",
-  "effect-app": "file:../../effect-app/libs/packages/prelude",
-  "@effect-app/fluent-extensions": "file:../../effect-app/libs/packages/fluent-extensions",
-  "@effect-app/infra": "file:../../effect-app/libs/packages/infra",
-  "@effect-app/infra-adapters": "file:../../effect-app/libs/packages/infra-adapters",
-  "@effect-app/schema": "file:../../effect-app/libs/packages/schema",
-  "@effect-app/vue": "file:../../effect-app/libs/packages/vue"
+  "@effect-app/core": "file:" + EFFECT_APP_LIBS + "/effect-app/libs/packages/core",
+  "@effect-app/eslint-codegen-model": "file:" + EFFECT_APP_LIBS + "/effect-app/libs/packages/eslint-codegen-model",
+  "effect-app": "file:" + EFFECT_APP_LIBS + "/effect-app/libs/packages/prelude",
+  "@effect-app/fluent-extensions": "file:" + EFFECT_APP_LIBS + "/effect-app/libs/packages/fluent-extensions",
+  "@effect-app/infra": "file:" + EFFECT_APP_LIBS + "/effect-app/libs/packages/infra",
+  "@effect-app/infra-adapters": "file:" + EFFECT_APP_LIBS + "/effect-app/libs/packages/infra-adapters",
+  "@effect-app/schema": "file:" + EFFECT_APP_LIBS + "/effect-app/libs/packages/schema",
+  "@effect-app/vue": "file:" + EFFECT_APP_LIBS + "/effect-app/libs/packages/vue",
+  ...packages.reduce((acc, p) => ({ ...acc, [p]: `file:${EFFECT_APP_LIBS}/node_modules/${p}` }), {})
 }
 
 fs.writeFileSync("./package.json", JSON.stringify(pj, null, 2))
--- packages/cli/src/unlink.ts
+++ packages/cli/src/unlink.ts
@@ -1,9 +1,10 @@
 import cp from "child_process"
 import fs from "fs"
+import { packages } from "./shared.js"
 
 const pj = (await import(process.cwd() + "/package.json", { assert: { type: "json" } })).default
 pj.resolutions = Object.entries(pj.resolutions as Record<string, string>).reduce((acc, [k, v]) => {
-  if (k.startsWith("@effect-app/") || k === "effect-app") return acc
+  if (k.startsWith("@effect-app/") || k === "effect-app" || packages.includes(k)) return acc
   acc[k] = v
   return acc
 }, {} as Record<string, string>)
