290a709660405e61e53de0b56e54d74c64e28a8c
Patrick Roza <contact@patrickroza.com>
2024-04-03 10:15:33 +0200

    housekeeping: update packages

--- packages/prelude/test/schema.test.ts
+++ packages/prelude/test/schema.test.ts
@@ -1,4 +1,4 @@
-import { generateFromArbitrary } from "@effect-app/infra/test.arbs"
+// import { generateFromArbitrary } from "@effect-app/infra/test.arbs"
 import { JSONSchema } from "@effect/schema"
 import { ReadonlyArray, S } from "effect-app"
 import { test } from "vitest"
@@ -6,7 +6,7 @@ import { test } from "vitest"
 const A = S.struct({ a: S.NonEmptyString255, email: S.nullable(S.Email) })
 test("works", () => {
   console.log(S.StringId.make())
-  console.log(generateFromArbitrary(S.A.make(A)).value)
+  // console.log(generateFromArbitrary(S.A.make(A)).value)
   console.log(S.AST.getTitleAnnotation(S.Email.ast))
   console.log(S.AST.getDescriptionAnnotation(S.Email.ast))
   console.log(S.AST.getJSONSchemaAnnotation(S.Email.ast))
