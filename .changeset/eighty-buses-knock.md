3de8bfbf461a60ca7d3013ff6a5b533da5c7a004
Patrick Roza <contact@patrickroza.com>
2024-09-27 08:37:54 +0200

    refactor: free up Effect.Service symbol.

---
+++ .changeset/eighty-buses-knock.md
@@ -0,0 +1,5 @@
+---
+"@effect-app/core": patch
+---
+
+free up Effect.Service symbol
--- packages/core/src/Effect.ts
+++ packages/core/src/Effect.ts
@@ -139,16 +139,16 @@ export function joinAll<E, A>(fibers: Iterable<Fiber.Fiber<A, E>>): Effect.Effec
   return Fiber.join(Fiber.all(fibers))
 }
 
-export type Service<T> = T extends Effect.Effect<infer S, any, any> ? S
+type ServiceA<T> = T extends Effect.Effect<infer S, any, any> ? S
   : T extends Context.Tag<any, infer S> ? S
   : never
-export type ServiceR<T> = T extends Effect.Effect<any, any, infer R> ? R
+type ServiceR<T> = T extends Effect.Effect<any, any, infer R> ? R
   : T extends Context.Tag<infer R, any> ? R
   : never
-export type ServiceE<T> = T extends Effect.Effect<any, infer E, any> ? E : never
-export type Values<T> = T extends { [s: string]: infer S } ? Service<S> : never
-export type ValuesR<T> = T extends { [s: string]: infer S } ? ServiceR<S> : never
-export type ValuesE<T> = T extends { [s: string]: infer S } ? ServiceE<S> : never
+type ServiceE<T> = T extends Effect.Effect<any, infer E, any> ? E : never
+// type Values<T> = T extends { [s: string]: infer S } ? ServiceA<S> : never
+type ValuesR<T> = T extends { [s: string]: infer S } ? ServiceR<S> : never
+type ValuesE<T> = T extends { [s: string]: infer S } ? ServiceE<S> : never
 
 /**
  * Due to tsplus unification (tsplus unify tag), when trying to use the Effect type in a type constraint
@@ -169,7 +169,7 @@ export interface EffectUnunified<R, E, A> extends Effect.Effect<R, E, A> {}
 export type LowerFirst<S extends PropertyKey> = S extends `${infer First}${infer Rest}` ? `${Lowercase<First>}${Rest}`
   : S
 export type LowerServices<T extends Record<string, Context.Tag<any, any> | Effect.Effect<any, any, any>>> = {
-  [key in keyof T as LowerFirst<key>]: Service<T[key]>
+  [key in keyof T as LowerFirst<key>]: ServiceA<T[key]>
 }
 
 /**
