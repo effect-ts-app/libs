8704dc2e9eb4b686f698771fd7919ff6ea7c2957
Patrick Roza <contact@patrickroza.com>
2024-09-28 21:19:55 +0200

    play with rcm

---
+++ packages/infra/src/api/internal/RequestContextMiddleware.ts
@@ -0,0 +1,68 @@
+import { Effect } from "effect-app"
+import { HttpMiddleware, HttpServerRequest, HttpServerResponse } from "effect-app/http"
+import { RequestId } from "effect-app/ids"
+import { NonEmptyString255 } from "effect-app/schema"
+import { RequestContext } from "../../RequestContext.js"
+import { RequestContextContainer } from "../../services/RequestContextContainer.js"
+import { setupExistingRequestContext } from "../setupRequest.js"
+
+export const RequestContextMiddleware = <Locale extends string = "en">(
+  config: {
+    supportedLocales: readonly Locale[]
+    defaultLocale: Locale
+  } = { supportedLocales: ["en" as Locale], defaultLocale: "en" as Locale }
+) =>
+  HttpMiddleware.make((app) =>
+    Effect.gen(function*() {
+      const req = yield* HttpServerRequest.HttpServerRequest
+
+      const currentSpan = yield* Effect.currentSpan
+      const parent = currentSpan?.parent ? currentSpan.parent.value : undefined
+      const start = new Date()
+      const desiredLocale = req.headers["x-locale"]
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const locale = desiredLocale && config.supportedLocales.includes(desiredLocale as any)
+        ? (desiredLocale as typeof config.supportedLocales[number])
+        : config.defaultLocale
+
+      const requestId = req.headers["request-id"]
+      const rootId = parent?.spanId
+        ? RequestId(parent.spanId)
+        : requestId
+        ? RequestId(requestId)
+        : RequestId.make()
+
+      const storeId = req.headers["x-store-id"]
+      const namespace = NonEmptyString255((storeId && (Array.isArray(storeId) ? storeId[0] : storeId)) || "primary")
+
+      const deviceId = req.headers["x-fe-device-id"]
+
+      const requestContext = new RequestContext({
+        id: currentSpan?.spanId ? RequestId(currentSpan.spanId) : RequestId.make(),
+        rootId,
+        name: NonEmptyString255(req.originalUrl), // set more detailed elsewhere
+        locale,
+        createdAt: start,
+        namespace,
+        sourceId: deviceId ? NonEmptyString255(deviceId) : undefined
+        // ...(context.operation.parentId
+        //   ? {
+        //     parent: new RequestContextParent({
+        //       id: RequestId(context.operation.parentId),
+        //       locale,
+        //       name: NonEmptyString255("API Request")
+        //     })
+        //   }
+        //   : {})
+      })
+      const rcc = yield* RequestContextContainer
+      yield* rcc.update((_) => requestContext)
+      const res = yield* setupExistingRequestContext(app, requestContext)
+
+      // TODO: how to set also on errors?
+      return HttpServerResponse.setHeaders(res, {
+        "request-id": requestContext.rootId,
+        "Content-Language": requestContext.locale
+      })
+    })
+  )
