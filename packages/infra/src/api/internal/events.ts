4ea1323bb0f4f98b989a2b80b703d7c8b0d6fdf3
Patrick Roza <contact@patrickroza.com>
2024-09-28 19:11:34 +0200

    fup

--- packages/infra/src/api/internal/events.ts
+++ packages/infra/src/api/internal/events.ts
@@ -7,41 +7,49 @@ import { HttpHeaders, HttpServerResponse } from "effect-app/http"
 const setRetry = Stream.succeed("retry: 10000")
 const keepAlive = Stream.schedule(Effect.succeed(":keep-alive"), Schedule.fixed(Duration.seconds(15)))
 
-export const makeSSE = <A extends { id: any }, E,R, SI>(events: Stream.Stream<{ evt: A, namespace: string}, E, R>, schema: S.Schema<A, SI>) => Effect
-  .gen(function*() {
-    yield* Effect.logInfo("$ start listening to events")
+export const makeSSE = <A extends { id: any }, E, R, SI, SR>(
+  events: Stream.Stream<{ evt: A; namespace: string }, E, R>,
+  schema: S.Schema<A, SI, SR>
+) =>
+  Effect
+    .gen(function*() {
+      yield* Effect.logInfo("$ start listening to events")
 
-    const enc = new TextEncoder()
+      const enc = new TextEncoder()
 
-    const eventStream = Stream.map(
-      events,
-      (_) => `id: ${_.evt.id}\ndata: ${JSON.stringify(S.encodeSync(schema)(_.evt))}`
-    )
+      const encode = S.encode(schema)
 
-    const stream = pipe(
-      setRetry,
-      Stream.merge(keepAlive),
-      Stream.merge(eventStream),
-      Stream.map((_) => enc.encode(_ + "\n\n"))
-    )
+      const eventStream = Stream.flatMap(
+        events,
+        (_) =>
+          encode(_.evt)
+            .pipe(Effect.andThen((evt) => `id: ${_.evt.id}\ndata: ${JSON.stringify(evt)}`))
+      )
 
-    const ctx = yield* Effect.context<R>()
-    const res = HttpServerResponse.stream(
-      stream
-        .pipe(
-          Stream.tapErrorCause(reportError("Request")),
-          Stream.provideContext(ctx)
-        ),
-      {
-        contentType: "text/event-stream",
-        headers: HttpHeaders.fromInput({
-          "content-type": "text/event-stream",
-          "cache-control": "no-cache",
-          "x-accel-buffering": "no",
-          "connection": "keep-alive" // if (req.httpVersion !== "2.0")
-        })
-      }
-    )
-    return res
-  })
-  .pipe(Effect.tapErrorCause(reportError("Request")), (_) => setupRequestContext(_, "events"))
+      const stream = pipe(
+        setRetry,
+        Stream.merge(keepAlive),
+        Stream.merge(eventStream),
+        Stream.map((_) => enc.encode(_ + "\n\n"))
+      )
+
+      const ctx = yield* Effect.context<R | SR>()
+      const res = HttpServerResponse.stream(
+        stream
+          .pipe(
+            Stream.tapErrorCause(reportError("Request")),
+            Stream.provideContext(ctx)
+          ),
+        {
+          contentType: "text/event-stream",
+          headers: HttpHeaders.fromInput({
+            "content-type": "text/event-stream",
+            "cache-control": "no-cache",
+            "x-accel-buffering": "no",
+            "connection": "keep-alive" // if (req.httpVersion !== "2.0")
+          })
+        }
+      )
+      return res
+    })
+    .pipe(Effect.tapErrorCause(reportError("Request")), (_) => setupRequestContext(_, "events"))
