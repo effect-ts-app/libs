56df0f32ecf68fff65639d730424f1543675abbc
Patrick Roza <contact@patrickroza.com>
2024-10-11 07:41:22 +0200

    adapt pipe extensions

--- packages/core/src/_ext/pipe.ts
+++ packages/core/src/_ext/pipe.ts
@@ -1,10 +1,23 @@
+/* eslint-disable unused-imports/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import { type Pipeable, pipeArguments } from "effect/Pipeable"
 
 declare global {
-  interface Object extends Pipeable {}
+  // Object makes trouble with Effect.Service etc
+  // interface Object extends Pipeable {}
+  interface Number extends Pipeable {}
+  interface Boolean extends Pipeable {}
+  interface String extends Pipeable {}
+  interface Array<T> extends Pipeable {}
+  interface ReadonlyArray<T> extends Pipeable {}
+  interface Map<K, V> extends Pipeable {}
+  interface ReadonlyMap<K, V> extends Pipeable {}
+  interface Set<T> extends Pipeable {}
+  interface ReadonlySet<T> extends Pipeable {}
+  interface Date extends Pipeable {}
+  interface Iterable<T, TReturn = any, TNext = any> extends Pipeable {}
 }
 
 Object.defineProperty(Object.prototype, "pipe", {
