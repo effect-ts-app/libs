e5ae174b0e4a77289e5b760c091d003631bbb30e
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-10-23 16:55:22 +0200

    it seems to work

--- packages/infra/examples/query.ts
+++ packages/infra/examples/query.ts
@@ -1,21 +1,39 @@
 import { RepositoryDefaultImpl2 } from "@effect-app/infra/services/RepositoryBase"
 import { Effect, flow, Layer, ManagedRuntime, S } from "effect-app"
-import { and, or, order, page, project, where } from "../src/services/query.js"
+import { and, page, where } from "../src/services/query.js"
 import { MemoryStoreLive } from "../src/services/Store/Memory.js"
 
 const str = S.Struct({ _tag: S.Literal("string"), value: S.String })
 const num = S.Struct({ _tag: S.Literal("number"), value: S.Number })
 const someUnion = S.Union(str, num)
 
-export class Something extends S.Class<Something>()({
+export class Something extends S.TaggedClass<Something>()("Something", {
   id: S.StringId.withDefault,
   displayName: S.NonEmptyString255,
   n: S.Date.withDefault,
   union: someUnion.pipe(S.withDefaultConstructor(() => ({ _tag: "string" as const, value: "hi" })))
 }) {}
+
+export class SomethingElse extends S.TaggedClass<SomethingElse>()("SomethingElse", {
+  id: S.StringId.withDefault,
+  banana: S.NonEmptyString255
+}) {}
+
+const Union = S.Union(Something, SomethingElse)
+
+export type Union = typeof Union.Type
+export namespace Union {
+  export type Encoded = Something.Encoded | SomethingElse.Encoded
+}
+
 export declare namespace Something {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface Encoded extends S.Schema.Encoded<typeof Something> {}
+}
+
+export declare namespace SomethingElse {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface Encoded extends S.Schema.Encoded<typeof SomethingElse> {}
 }
 
 const items = [
@@ -25,12 +43,14 @@ const items = [
     displayName: S.NonEmptyString255("Riley"),
     n: new Date("2020-01-01T00:00:00Z"),
     union: { _tag: "number", value: 1 }
-  })
+  }),
+  new SomethingElse({ banana: S.NonEmptyString255("Banana") }),
+  new SomethingElse({ banana: S.NonEmptyString255("Banana2") })
 ]
 
 class SomethingRepo extends RepositoryDefaultImpl2<SomethingRepo>()(
-  "Something",
-  Something,
+  "Union",
+  Union,
   { idKey: "id" }
 ) {
   static readonly Test = Layer
@@ -45,14 +65,17 @@ class SomethingRepo extends RepositoryDefaultImpl2<SomethingRepo>()(
 
 const program = Effect.gen(function*() {
   const r = yield* SomethingRepo.query(flow(
-    where("displayName", "Verona"),
-    or(
-      where("displayName", "Riley"),
-      and("n", "gt", "2021-01-01T00:00:00Z") // TODO: work with To type translation, so Date?
-    ),
-    order("displayName"),
-    page({ take: 1 }),
-    project(S.Struct(Something.pick("id", "displayName")))
+    where("id", "Verona"),
+    and("_tag", "Something"),
+    // or(
+    //   where("displayName", "Riley"),
+    //   and("n", "gt", "2021-01-01T00:00:00Z"), // TODO: work with To type translation, so Date?
+    //   and("_tag", "Something")
+    // ),
+    // order("displayName")
+    page({ take: 1 })
+    // one
+    // project(S.Struct(Something.pick("id", "displayName")))
   ))
   console.log("$$ result", r)
 })
