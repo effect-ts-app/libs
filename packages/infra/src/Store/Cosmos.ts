61e1853c6609bc9f2dc080c2edc05831f2946f82
Patrick Roza <contact@patrickroza.com>
2024-10-26 14:38:48 +0200

    fix typo

--- packages/infra/src/Store/Cosmos.ts
+++ packages/infra/src/Store/Cosmos.ts
@@ -252,7 +252,7 @@ function makeCosmosStore({ prefix }: StorageConfig) {
                     name,
                     importedMarkerId,
                     defaultValues,
-                    f.select!.map((_) => (_ as any) === idKey ? "id" : _) as NonEmptyReadonlyArray<string> | undefined,
+                    f.select?.map((_) => (_ as any) === idKey ? "id" : _) as NonEmptyReadonlyArray<string> | undefined,
                     f.order as NonEmptyReadonlyArray<{ key: string; direction: "ASC" | "DESC" }> | undefined,
                     skip,
                     limit
