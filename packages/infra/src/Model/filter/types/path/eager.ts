1755b4df4b36499a75ce6986e09f459f8bc80ebd
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-11-01 10:56:07 +0100

    remove stuff that seems to be useless, ident

--- packages/infra/src/Model/filter/types/path/eager.ts
+++ packages/infra/src/Model/filter/types/path/eager.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { FieldValues } from "../fields.js"
 import type { BrowserNativeObject, Primitive } from "../utils.js"
 
@@ -34,39 +35,6 @@ export type Path<T> = T extends ReadonlyArray<infer V> ? IsTuple<T> extends true
 export type FieldPath<TFieldValues extends FieldValues> = Path<TFieldValues>
 
 /**
- * Helper type for recursively constructing paths through a type.
- * See {@link ArrayPath}
- */
-type ArrayPathImpl<K extends string | number, V> = V extends
-  | Primitive
-  | BrowserNativeObject ? never
-  : V extends ReadonlyArray<infer U> ? U extends Primitive | BrowserNativeObject ? never
-    : `${K}` | `${K}.${ArrayPath<V>}`
-  : `${K}.${ArrayPath<V>}`
-
-/**
- * Type which eagerly collects all paths through a type which point to an array
- * type.
- * @typeParam T - type which should be introspected
- * @example
- * ```
- * Path<{foo: {bar: string[], baz: number[]}}> = 'foo.bar' | 'foo.baz'
- * ```
- */
-export type ArrayPath<T> = T extends ReadonlyArray<infer V> ? IsTuple<T> extends true ? {
-      [K in TupleKeys<T>]-?: ArrayPathImpl<K & string, T[K]>
-    }[TupleKeys<T>]
-  : ArrayPathImpl<ArrayKey, V>
-  : {
-    [K in keyof T]-?: ArrayPathImpl<K & string, T[K]>
-  }[keyof T]
-
-/**
- * See {@link ArrayPath}
- */
-export type FieldArrayPath<TFieldValues extends FieldValues> = ArrayPath<TFieldValues>
-
-/**
  * Type to evaluate the type which the given path points to.
  * @typeParam T - deeply nested type which is indexed by the path
  * @typeParam P - path into the deeply nested type
@@ -76,18 +44,28 @@ export type FieldArrayPath<TFieldValues extends FieldValues> = ArrayPath<TFieldV
  * PathValue<[number, string], '1'> = string
  * ```
  */
-export type PathValue<T, P extends Path<T> | ArrayPath<T>> = T extends any
-  ? P extends `${infer K}.${infer R}` ? K extends keyof T ? R extends Path<T[K]> ? PathValue<T[K], R>
-      : never
-    : K extends `${ArrayKey}` ? T extends ReadonlyArray<infer V> ? PathValue<V, R & Path<V>>
-      : never
+/* dprint-ignore-start */
+export type PathValue<T, P extends Path<T>> =
+  T extends any
+    ? P extends `${infer K}.${infer R}`
+      ? K extends keyof T
+        ? R extends Path<T[K]>
+          ? PathValue<T[K], R>
+          : never
+        : K extends `${ArrayKey}`
+          ? T extends ReadonlyArray<infer V>
+            ? PathValue<V, R & Path<V>>
+            : never
+          : never
+      : P extends keyof T
+        ? T[P]
+        : P extends `${ArrayKey}`
+          ? T extends ReadonlyArray<infer V>
+            ? V
+            : never
+          : never
     : never
-  : P extends keyof T ? T[P]
-  : P extends `${ArrayKey}` ? T extends ReadonlyArray<infer V> ? V
-    : never
-  : never
-  : never
-
+/* dprint-ignore-end */
 /**
  * See {@link PathValue}
  */
@@ -95,14 +73,6 @@ export type FieldPathValue<
   TFieldValues extends FieldValues,
   TFieldPath extends FieldPath<TFieldValues>
 > = PathValue<TFieldValues, TFieldPath>
-
-/**
- * See {@link PathValue}
- */
-export type FieldArrayPathValue<
-  TFieldValues extends FieldValues,
-  TFieldArrayPath extends FieldArrayPath<TFieldValues>
-> = PathValue<TFieldValues, TFieldArrayPath>
 
 /**
  * Type to evaluate the type which the given paths point to.
